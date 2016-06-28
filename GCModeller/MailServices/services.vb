711e8ea00c7ad8dd57d5d2af8e9205f87f889b61
xieguigang <xie.guigang@gmail.com>
2016-06-29 05:35:44 +0800

    add license info

--- GCModeller/MailServices/services.vb
+++ GCModeller/MailServices/services.vb
@@ -1,4 +1,31 @@
-﻿Imports Microsoft.VisualBasic.Net.Mailto
+﻿#Region "Microsoft.VisualBasic::97343f03c7880de85dc0e766053e9e25, ..\GCModeller\MailServices\services.vb"
+
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xieguigang (xie.guigang@live.com)
+    ' 
+    ' Copyright (c) 2016 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+Imports Microsoft.VisualBasic.Net.Mailto
 
 ''' <summary>
 ''' services@gcmodeller.org
@@ -11,3 +38,4 @@ Module MailServices
         Return Net.Mailto.EMailClient.QQMail("analysis-services@gcmodeller.org", AppKey)
     End Function
 End Module
+
