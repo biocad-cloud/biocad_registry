f2af6a08d3b7fc9af2b88f01c0ea999bea05e9de
xieguigang <xie.guigang@gmail.com>
2017-04-28 23:27:50 +0800

    add web app module

---
+++ biostack/KEGG.vb
@@ -0,0 +1,13 @@
+﻿Imports SMRUCC.WebCloud.HTTPInternal.AppEngine
+Imports SMRUCC.WebCloud.HTTPInternal.Platform
+
+Public Class KEGG : Inherits WebApp
+
+    Public Sub New(main As PlatformEngine)
+        MyBase.New(main)
+    End Sub
+
+    Public Overrides Function Page404() As String
+        Return ""
+    End Function
+End Class
