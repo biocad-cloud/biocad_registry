abb3c8dcae53002a0a8a22ad17310c8b3283827f
xieguigang <xie.guigang@gmail.com>
2017-05-02 20:20:17 +0800

    basic d3 network, required improvements on the styles

--- biostack/Debugger.vb
+++ biostack/Debugger.vb
@@ -1,8 +1,18 @@
-﻿Imports SMRUCC.WebCloud.HTTPInternal
+﻿Imports SMRUCC.WebCloud.d3js.Network
+Imports SMRUCC.WebCloud.HTTPInternal
+Imports NetGraph = Microsoft.VisualBasic.Data.visualize.Network.FileStream.Network
 
 Module Debugger
 
     Sub Main()
+        Call D3NetworkTest
         Call DebuggerAPI.Start(wwwroot:="../wwwroot", threads:=0)
+    End Sub
+
+    Sub D3NetworkTest
+        Call NetGraph _
+            .Load("C:\Users\xieguigang\OneDrive\4.7\sqq\drugbank-drug-pathway.knowledge_network") _
+            .FromNetwork _
+            .SaveTo("./net.json")
     End Sub
 End Module
