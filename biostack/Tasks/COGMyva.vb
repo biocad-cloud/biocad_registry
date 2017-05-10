c5ac4692e405683b808d3da5971a86153a848704
xieguigang <xie.guigang@gmail.com>
2017-05-10 21:52:47 +0800

    task model

---
+++ biostack/Tasks/COGMyva.vb
@@ -0,0 +1,13 @@
+﻿Imports SMRUCC.WebCloud.HTTPInternal.Platform
+
+Public Class COGMyva : Inherits TaskModel
+
+    Protected Overrides Function contents() As String()
+         Return {
+            "Blast+ myva database search", 
+            "Export blastp table", 
+            "COG annotation", 
+            "Data Plot"
+        }
+    End Function
+End Class
