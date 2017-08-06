580c3f20902d600eac67dc5dc5fbb95013526720
xieguigang <xie.guigang@gcmodeller.org>
2017-08-06 15:30:59 +0800

    execute app write success

--- biostack/WebApp.vb
+++ biostack/WebApp.vb
@@ -1,5 +1,6 @@
 ﻿Imports Oracle.LinuxCompatibility.MySQL
 Imports SMRUCC.WebCloud.DataCenter
+Imports SMRUCC.WebCloud.DataCenter.mysql
 
 Module WebApp
 
@@ -15,6 +16,9 @@ Module WebApp
             ' 检查WebApp的注册情况
             Dim apps = AppHelper.LoadData(Of BiostackApps)
 
+            For Each app As app In apps
+                Call .ExecReplace(app)
+            Next
         End With
     End Sub
 End Module
