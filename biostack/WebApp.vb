a9ca2ecd873fb2d7f935dfe0d2719773a24ecd31
xieguigang <xie.guigang@gcmodeller.org>
2017-08-06 11:49:01 +0800

    add config code

--- biostack/WebApp.vb
+++ biostack/WebApp.vb
@@ -1,16 +1,20 @@
-﻿Imports Oracle.LinuxCompatibility.MySQL
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Oracle.LinuxCompatibility.MySQL
 Imports SMRUCC.WebCloud.DataCenter
+Imports Microsoft.VisualBasic.Terminal
 Imports SMRUCC.WebCloud.DataCenter.mysql
+Imports sys = Microsoft.VisualBasic.Terminal.STDIO
 
-Module WebApp
+<RunDllEntryPoint(NameOf(WebApp))> Module WebApp
+
+    Sub New()
+        Call Settings.Session.Initialize()
+    End Sub
 
     Sub Main()
-        Console.WriteLine("Call Main")
-
-        ' 进行一些数据库的初始化工作
-
         With New MySQL
 
+            ' 进行一些数据库的初始化工作
             Call .init
 
             ' 检查WebApp的注册情况
@@ -19,6 +23,25 @@ Module WebApp
             For Each app As app In apps
                 Call .ExecReplace(app)
             Next
+        End With
+    End Sub
+
+    Public Sub RunConfig()
+        Dim readString = Function(s$, ByRef result$)
+                             result = s
+                             Return Not s.StringEmpty
+                         End Function
+
+        Dim repo$ = sys.Read("Input the directory location that where the repository data is?", readString, HOME & "/repo/")
+        Dim blast = sys.Read("Input the directory location of the NCBI blast+ suite", readString, HOME & "/blast+/")
+
+        With Settings.SettingsFile
+
+            .RepositoryRoot = repo
+            .BlastBin = blast
+
+            Call .Save()
+
         End With
     End Sub
 End Module
