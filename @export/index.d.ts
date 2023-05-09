9ecef8d770e834384af99496526dcfd90e96e071
xieguigang <xie.guigang@gcmodeller.org>
2023-05-09 21:46:03 +0800

    build packages

---
+++ @export/index.d.ts
@@ -0,0 +1,11 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=biocad_registry
+
+declare namespace biocad_registry {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+}
