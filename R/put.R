7e4b35ab5d6a06891b5d892fe6b3c91b575a8729
xieguigang <xie.guigang@gcmodeller.org>
2023-05-09 22:09:16 +0800

    Update put.R

--- R/put.R
+++ R/put.R
@@ -1,7 +1,16 @@
+#' Create a new taxonomic group in the registry
+#' 
+#' @return the unique reference id of the new taxonomic group
+#'   in the registry.
+#' 
 const put.taxonomic_group = function(name, note = "") {
     const base = getOption("biocad");
     const url  = `${base}/registry/put/taxonomic/`;
     const resp = requests.post(url, list(name, note)) |> http::content();
 
+    # view of the taxonomic object put 
+    # response
     str(resp);
+
+    return(resp$info$id);
 }
