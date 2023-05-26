6786c2a4bdce56464118699ef8f578e3760c512e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-27 02:29:31 +0800

    Create get_sites.R

---
+++ test/regprecise/get_sites.R
@@ -0,0 +1,26 @@
+imports "package_utils" from "devkit";
+imports ["jQuery", "Html", "http"] from "webKit";
+
+package_utils::attach(`${@dir}/../../`);
+
+setwd(@dir);
+
+require(JSON);
+
+const alldata = biocad_registry::pull_taxonomic();
+const local = http.cache("./cache/");
+
+for(tax in alldata) {
+    const genomes = biocad_registry::pull_taxonomic(tax$id);
+
+    # str(genomes);
+
+    for(genome in genomes) {
+        str(genome);
+
+        const gene_text = requests.get(`https://regprecise.lbl.gov/ExportServlet?type=gene&genomeId=${genome$id}`, cache = local);
+        const motif_text = requests.get(`https://regprecise.lbl.gov/ExportServlet?type=site&genomeId=${genome$id}`, cache = local);
+
+        sleep(3);
+    }
+}
