a39ec9f76708c746c49d164509c6c1a291270079
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-26 23:22:54 +0800

    fix of the web configs

--- R/put.R
+++ R/put.R
@@ -5,7 +5,7 @@
 #' 
 const put.taxonomic_group = function(name, note = "") {
     const base = getOption("biocad");
-    const url  = `${base}/registry/put/taxonomic/`;
+    const url  = `${base}/put/taxonomic/`;
     const resp = requests.post(url, list(name, note)) |> http::content();
 
     # view of the taxonomic object put 
