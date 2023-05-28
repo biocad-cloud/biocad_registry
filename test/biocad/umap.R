c59cb2b8b5d000465460d193a2df4a4090666a58
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-28 18:08:43 +0800

    Update umap.R

---
+++ test/biocad/umap.R
@@ -0,0 +1,15 @@
+require(umap);
+
+setwd(@dir);
+
+let data = read.csv("./motif_graph.csv", row.names = 1, check.names = FALSE);
+let labels = data$class;
+
+data[, "class"] = NULL;
+
+let dim3 = umap(data, dimension = 3);
+let result = as.data.frame(dim3$umap, labels = dim3$labels);
+
+result$class = labels;
+
+write.csv(result, file = "./umap3.csv", row.names = TRUE);
