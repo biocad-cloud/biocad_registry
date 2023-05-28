acf7ae93d02825d88d0c7c0f3a12710786b58e96
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-28 13:54:21 +0800

    pull motif sites dfasta

--- R/pull.R
+++ R/pull.R
@@ -15,3 +15,29 @@ const pull_taxonomic = function(id = NULL) {
         stop(resp$info);
     }
 }
+
+#' Fetch all motif family
+#'  
+const pull_motif_family = function(genome_id = NULL) {
+    const base = getOption("biocad");
+    const url = {
+        if (is.null(genome_id)) {
+            `${base}/pull/motif_site_family/`;
+        } else {
+            `${base}/pull/motif_site_family/?genome=${genome_id}`;
+        };
+    }
+    const resp = requests.get(url) |> http::content();
+
+    if (resp$code == 0) {
+        const data = resp$info;
+        const df = data.frame(
+            family = data@family,
+            nsize = as.integer(data@nsize)
+        );
+
+        df;
+    } else {
+        stop(resp$info);
+    }
+}
--- test/biocad/get_motifs.R
+++ test/biocad/get_motifs.R
@@ -6,3 +6,18 @@ package_utils::attach(`${@dir}/../../`);
 motifs = biocad_registry::get_motif_sites(family = "PsrA"); 
 
 print(motifs);
+
+motif_familys = biocad_registry::pull_motif_family();
+i = motif_familys$nsize > 5;
+motif_familys = motif_familys[i, ];
+i = order(motif_familys$nsize, decreasing = TRUE);
+motif_familys = motif_familys[i, ];
+
+print(motif_familys);
+
+for(family in motif_familys$family) {
+    motifs = biocad_registry::get_motif_sites(family = family); 
+    print(motifs);
+    fa = as.fasta(data.frame(title = rownames(motifs), sequence = motifs$motif_site));
+    write.fasta(fa, file = `${@dir}/exports/${normalizeFileName(family)}.fa`);
+}
