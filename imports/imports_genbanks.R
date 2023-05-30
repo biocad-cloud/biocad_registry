440b044c79a5d91853633ba2d6578acbbd99c1f2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-30 22:07:31 +0800

    Create imports_genbanks.R

---
+++ imports/imports_genbanks.R
@@ -0,0 +1,21 @@
+require(GCModeller);
+
+options(http.cache_dir = @dir);
+
+tax = taxonomy_search("Magnetospirillum magneticum AMB-1");
+
+str(tax);
+
+assm_list = reference_genome(ncbi_taxid =tax );
+assm_list = first(assm_list);
+
+str(assm_list);
+
+let id = assm_list$assembly$assembly_accession;
+
+str(id);
+
+
+let gb = fetch_genbank(accession_id = id);
+
+print(gb);
