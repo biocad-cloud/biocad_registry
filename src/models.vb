9aa2e076f3f7800f1f09c81b478a6f6d1b5093fc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-11 00:05:51 +0800

    export mysql models

---
+++ src/models.vb
@@ -0,0 +1,19 @@
+﻿Imports biocad_registry.biocad_registry
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("models")>
+<RTypeExport("reaction_node", GetType(reaction_node))>
+<RTypeExport("reaction_graph", GetType(reaction_graph))>
+<RTypeExport("molecules", GetType(molecules))>
+<RTypeExport("dblinks", GetType(dblinks))>
+<RTypeExport("subcellular_compartments", GetType(subcellular_compartments))>
+<RTypeExport("subcellular_locations", GetType(subcellular_locations))>
+<RTypeExport("vocabulary", GetType(vocabulary))>
+<RTypeExport("pathway", GetType(pathway))>
+Module models
+
+    Public Sub Main()
+
+    End Sub
+End Module
