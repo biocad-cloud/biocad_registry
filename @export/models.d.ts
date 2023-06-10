a0f5422e9c73b800c52ed1589c9f023580ccad38
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-11 00:15:27 +0800

    Create models.d.ts

---
+++ @export/models.d.ts
@@ -0,0 +1,8 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "models" from "biocad_registry";
+//
+// ref=biocad_registry.models@biocad_registry, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+*/
