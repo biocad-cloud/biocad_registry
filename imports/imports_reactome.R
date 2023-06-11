3910925881fe8fe81681dcceabd65082a2c84713
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-11 14:36:30 +0800

    create compartments

--- imports/imports_reactome.R
+++ imports/imports_reactome.R
@@ -10,6 +10,7 @@ const repo = "E:\UniProt\all_species.3.1.sbml";
 const models = list.files(repo, pattern = "*.sbml");
 
 let reaction_node = [];
+let subcellular_compartments = [];
 
 print(basename(models));
 
@@ -32,7 +33,15 @@ for(file in models[1:3]) {
             add_time = now(),
             n_left = length(r$reactants),
             n_right = length(r$products),
-            n_regulator = length(r$modifiers)
+            n_regulator = length(r$modifiers),
+            note = r$notes
+        );
+    }));
+    subcellular_compartments = append(subcellular_compartments, sapply(compartments, function(c) {
+        new subcellular_compartments(
+            id = c$id,
+            name = c$name,
+            add_time = now(),
         );
     }));
 
@@ -40,4 +49,7 @@ for(file in models[1:3]) {
     invisible(NULL);
 }
 
-mysql::dump_inserts(reaction_node, dir = `${@dir}/reactions/`);
+reaction_node
+|> append(subcellular_compartments)
+|> mysql::dump_inserts(dir = `${@dir}/reactions/`)
+;
