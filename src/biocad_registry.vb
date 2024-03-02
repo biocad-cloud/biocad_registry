ac3a08ae299e1fead17029ccbab64e64d31e9ebd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-02 16:02:44 +0800

    add model

---
+++ src/biocad_registry.vb
@@ -0,0 +1,9 @@
+﻿Imports Oracle.LinuxCompatibility.MySQL
+Imports Oracle.LinuxCompatibility.MySQL.Uri
+
+Public Class biocad_registry : Inherits IDatabase
+
+    Sub New(mysql As ConnectionUri)
+        Call MyBase.New(mysql)
+    End Sub
+End Class
