cae59e133cdfc9f6cc927ef8de35e93134f3cffa
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-03 03:16:45 +0800

    Update imports_uniprot.R

--- test/imports_uniprot.R
+++ test/imports_uniprot.R
@@ -5,6 +5,10 @@ imports "uniprot" from "seqtoolkit";
 let biocad_registry = open_registry("root", 123456);
 let uniprot = parseUniProt(readText(file.path(@dir,"uniprotkb_taxonomy_id_2_AND_model_organ_2024_08_02.xml")));
 
-uniprot = as.data.frame(uniprot);
+# uniprot = as.data.frame(uniprot);
 
-print(uniprot);
+# print(uniprot);
+
+for(let prot in tqdm(uniprot)) {
+    
+}
