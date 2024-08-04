8b7a5b1fc1c3c01f7d6560bcd1d20943c694c32b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-04 21:20:19 +0800

    inspect of the gene model data

--- R/repository/genbank.R
+++ R/repository/genbank.R
@@ -5,6 +5,24 @@ const imports_genebank = function(biocad_registry, genebank) {
     let genes = genebank |> enumerateFeatures(keys = ["gene","tRNA","ncRNA","rRNA"]);
 
     for(gene in tqdm(genes)) {
-        str(gene |> featureMeta());
+        let nt_seq = as.fasta(gene);
+
+        gene <- featureMeta(gene );
+
+        let locus_tag = gene$locus_tag;
+        let gene_synonym = strsplit(gene$gene_synonym, ";\s*"); 
+        let db_xref =   tagvalue(gene$db_xref ,
+                delimiter = ":",
+                trim_value = TRUE,
+                as_list = TRUE,
+                union_list = TRUE);
+        
+        gene <- gene$gene;
+
+        print(nt_seq);
+        str(gene);
+        str(gene_synonym);
+        str(db_xref);
+        stop(); 
     }
 }
