c2f2d67e612db31af13f6e952493274565a5387b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-04 21:48:00 +0800

    Update genbank.R

--- R/repository/genbank.R
+++ R/repository/genbank.R
@@ -3,6 +3,7 @@ imports "GenBank" from "seqtoolkit";
 const imports_genebank = function(biocad_registry, genebank) {
     let term_gene = biocad_registry |> gene_term();
     let genes = genebank |> enumerateFeatures(keys = ["CDS","tRNA","ncRNA","rRNA"]);
+    let gene_pool =  biocad_registry |> table("molecule");
 
     for(gene in tqdm(genes)) {
         let nt_seq = as.fasta(gene);
@@ -19,6 +20,30 @@ const imports_genebank = function(biocad_registry, genebank) {
                 union_list = TRUE);
         
         gene <- gene$gene;
+
+        let mol = gene_pool   |> where(type = term_gene ,
+            xref_id =locus_tag) |> find();
+
+        if (is.null(mol)) {
+            gene_pool |> add(
+                xref_id = locus_tag,
+                name = gene,
+                mass = bioseq.fasta::mass(nt_seq, type="DNA"),
+                type =  term_gene,
+                formula = seq_formula(nt_seq, type="DNA"),
+                parent = 0,
+                note = note_str
+            );
+
+            mol =gene_pool  |> where(type =term_gene,
+                xref_id = locus_tag ) |> find();
+        }
+
+        if (is.null(mol)) {
+            # error while add new metabolite
+            next;
+        }
+
 
         print(note_str);
         print(nt_seq);
