6709b2d06968b7ff94b4886960db25b678b9038b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-05 18:37:23 +0800

    run package msbuikld

--- @export/index.d.ts
+++ @export/index.d.ts
@@ -13,6 +13,9 @@ declare namespace biocad_registry {
    function gene_term(biocad_registry: any): object;
    /**
    */
+   function imports_chebi(biocad_registry: any, chebi: any): object;
+   /**
+   */
    function imports_genebank(biocad_registry: any, genebank: any): object;
    /**
    */
