c344d28fa179f7dc5ce79a520c72c6642dd5c615
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-09 03:08:21 +0800

    Update chebi.R

--- R/repository/chebi.R
+++ R/repository/chebi.R
@@ -1,5 +1,11 @@
 #' imports the metabolite set from chebi database
 #' 
 const imports_chebi = function(biocad_registry, chebi) {
-    
+    let term_metabolite = biocad_registry::metabolite_term(biocad_registry);
+    let entity_metabolite = biocad_registry::molecule_entity(biocad_registry);
+    let db_xrefs = biocad_registry |> table("db_xrefs");
+    let compartments = biocad_registry |> table("subcellular_compartments");
+    let location_link = biocad_registry |> table("subcellular_location");
+    let metabolite = biocad_registry |> table("molecule");
+    let seq_graph = biocad_registry |> table("sequence_graph");
 }
