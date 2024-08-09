b89d7367be5c4f203e237bd389ba4cc1b318f51f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-09 20:46:09 +0800

    common method for imports compound data via a unify data model

--- R/repository/chebi.R
+++ R/repository/chebi.R
@@ -13,8 +13,36 @@ const imports_chebi = function(biocad_registry, chebi) {
 
     chebi = massbank::extract_chebi_compounds(chebi);
 
-    for(let mol in tqdm(chebi)) {
-        mol = as.list(mol);
+    for(let meta in tqdm(chebi)) {
+        let xref_id = [meta]::ID;
+        let mol = metabolite |> where(type = term_metabolite ,
+            xref_id = xref_id ) |> find();
+
+        meta = as.list(meta);
+
+        if (is.null(mol)) {
+            metabolite |> add(
+                xref_id = xref_id,
+                name = meta$name,
+                mass = formula::eval(meta$formula),
+                type =  term_metabolite,
+                formula = meta$formula,
+                parent = 0,
+                note = meta$description  
+            );
+
+            mol =metabolite  |> where(type =term_metabolite,
+                xref_id = xref_id ) |> find();
+        }
+
+        if (is.null(mol)) {
+            # error while add new metabolite
+            next;
+        }
+
+        let xrefs = meta$xref;
+        
+
         str(mol);
         stop();
     }
--- test/imports_chebi.R
+++ test/imports_chebi.R
@@ -1,2 +1,8 @@
 require(biocad_registry);
 
+imports "OBO" from "annotationKit";
+
+let chebi = open.obo("G:\biocad_registry\test\chebi.obo");
+let biocad_registry = open_registry("root", 123456);
+
+biocad_registry |> imports_chebi(chebi);
