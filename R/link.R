59a1d4cad8a0978b372cc96a207ba6e20acb9f29
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-10 23:23:46 +0800

    check of the metabolite

--- R/link.R
+++ R/link.R
@@ -3,18 +3,27 @@
 #' @return an integer id of the target molecule, NULL when object not found.
 #' 
 const get_molecule_by_dbxref = function(registry, db_xref, dbname = NULL) {
-    if (nchar(dbname) > 0) {
-        let db_key = registry |> vocabulary_id(dbname, "External Database");
-        let xrefs = registry |> table("db_xrefs") |> where(
-            db_key = db_key, xref = db_xref, type in molecule_terms(registry)
-        ) |> find();
+    let xrefs = registry |> table("db_xrefs");
+    let molecules = molecule_terms(registry);
 
-        xrefs$obj_id;
+    xrefs = {
+        if (nchar(dbname) > 0) {
+            xrefs = xrefs |> where(
+                db_key = registry |> vocabulary_id(dbname, "External Database"), 
+                xref = db_xref, 
+                type in molecules
+            );
+        } else {
+            xrefs = xrefs |> where(
+                xref = db_xref, 
+                type in molecules
+            );
+        }
+    }
+
+    if (is.null(xrefs = xrefs |> find())) {
+        NULL;
     } else {
-        let xrefs = registry |> table("db_xrefs") |> where(
-            xref = db_xref, type in molecule_terms(registry)
-        ) |> find();
-
         xrefs$obj_id;
     }
 }
---
+++ R/repository/metabolite/check_metabolite.R
@@ -0,0 +1,41 @@
+#' check of the metabolite is existsed inside database
+#' 
+#' @return an integer unique id of the compound that matched with the 
+#'   compound xrefs information, NULL will be returns if the molecule 
+#'   metabolite data has not been found.
+#' 
+const check_metabolite = function(biocad_registry, compound) {
+    let xrefs = as.list(compound$xref);
+    let exact_mass = formula::eval(compound$formula);
+
+    # removes all molecular strucutre data
+    xrefs$InChIkey <- NULL;
+    xrefs$InChI    <- NULL;
+    xrefs$SMILES   <- NULL;
+    xrefs$extras   <- NULL;
+
+    xrefs <- unlist(xrefs);
+    xrefs <- xrefs[nchar(xrefs) > 0];
+    
+    if (lenth(xrefs) > 0) {
+        compound = biocad_registry 
+        |> table("db_xrefs") 
+        |> left_join("molecule") 
+        |> on(molecule.id = db_xrefs.obj_id) 
+        |> where(
+            mass between [exact_mass - 1, exact_mass + 1],
+            db_xrefs.type in molecules,
+            xref in xrefs
+        ) 
+        |> find("molecule.*")
+        ;
+
+        if (is.null(compound)) {
+            NULL;
+        } else {
+            compound$id;
+        }
+    } else {
+        NULL;
+    }
+}
