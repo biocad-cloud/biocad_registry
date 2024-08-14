0ac8ebae1e33b5b9d690c891b6bd48f7cd358615
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-14 21:43:38 +0800

    Create link_gene_proteins.R

---
+++ R/links/link_gene_proteins.R
@@ -0,0 +1,9 @@
+#' Link the gene and protein parent/child relationship
+#' 
+#' @details probabily there is no needs for links the protein back to its 
+#'   source gene object, due to the reason of gene sequence could be reverse
+#'   translates from the protein sequence.
+#' 
+const link_gene_proteins = function(biocad_registry) {
+
+}
