ad445625b3a8607da2547c04254af78e4dcd909c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-25 19:35:42 +0800

    split function into multiple parts

--- src/biocad_registry.vb
+++ src/biocad_registry.vb
@@ -16,30 +16,34 @@ Public Class biocad_registry : Inherits biocad_registryModel.db_mysql
         SyncLock cache
             Return cache.ComputeIfAbsent($"{Strings.LCase(category)}:{Strings.LCase(term)}",
                 lazyValue:=Function()
-                               Dim check = m_vocabulary.where(
-                                  field("category") = category,
-                                  field("term") = term
-                               ).find(Of biocad_registryModel.vocabulary)
-
-                               If check Is Nothing Then
-                                   Call m_vocabulary.add(
-                                       field("category") = category,
-                                       field("term") = term,
-                                       field("note") = description
-                                   )
-                                   check = m_vocabulary.where(
-                                      field("category") = category,
-                                      field("term") = term
-                                   ).find(Of biocad_registryModel.vocabulary)
-                               End If
-
-                               If check Is Nothing Then
-                                   Throw New InvalidProgramException
-                               Else
-                                   Return check.id
-                               End If
+                               Return queryVocabulary(term, category, description)
                            End Function)
         End SyncLock
+    End Function
+
+    Private Function queryVocabulary(term As String, category As String, description As String) As UInteger
+        Dim check = m_vocabulary.where(
+            field("category") = category,
+            field("term") = term
+        ).find(Of biocad_registryModel.vocabulary)
+
+        If check Is Nothing Then
+            Call m_vocabulary.add(
+                field("category") = category,
+                field("term") = term,
+                field("note") = description
+            )
+            check = m_vocabulary.where(
+                field("category") = category,
+                field("term") = term
+            ).find(Of biocad_registryModel.vocabulary)
+        End If
+
+        If check Is Nothing Then
+            Throw New InvalidProgramException
+        Else
+            Return check.id
+        End If
     End Function
 
     Public Function getSubcellularLocation(name As String, topology As String) As UInteger
@@ -47,26 +51,30 @@ Public Class biocad_registry : Inherits biocad_registryModel.db_mysql
         SyncLock cache
             Return cache.ComputeIfAbsent(name.ToLower,
                 lazyValue:=Function()
-                               Dim check = m_subcellular_compartments _
-                                   .where(field("compartment_name") = name) _
-                                   .find(Of biocad_registryModel.subcellular_compartments)
-
-                               If check Is Nothing Then
-                                   Call m_subcellular_compartments.add(
-                                     field("compartment_name") = name,
-                                     field("topology") = topology
-                                   )
-                                   check = m_subcellular_compartments _
-                                      .where(field("compartment_name") = name) _
-                                      .find(Of biocad_registryModel.subcellular_compartments)
-                               End If
-
-                               If check Is Nothing Then
-                                   Throw New InvalidProgramException
-                               Else
-                                   Return check.id
-                               End If
+                               Return querySubcellularLocation(name, topology)
                            End Function)
         End SyncLock
+    End Function
+
+    Private Function querySubcellularLocation(name As String, topology As String) As UInteger
+        Dim check = m_subcellular_compartments _
+            .where(field("compartment_name") = name) _
+            .find(Of biocad_registryModel.subcellular_compartments)
+
+        If check Is Nothing Then
+            Call m_subcellular_compartments.add(
+                field("compartment_name") = name,
+                field("topology") = topology
+            )
+            check = m_subcellular_compartments _
+               .where(field("compartment_name") = name) _
+               .find(Of biocad_registryModel.subcellular_compartments)
+        End If
+
+        If check Is Nothing Then
+            Throw New InvalidProgramException
+        Else
+            Return check.id
+        End If
     End Function
 End Class
