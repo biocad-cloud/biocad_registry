e4f49151aa81dde333c0b31f69b3cea044800a3c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-29 18:30:46 +0800

    Update imports_uniprot_full.R

--- test/imports_uniprot_full.R
+++ test/imports_uniprot_full.R
@@ -3,4 +3,4 @@ require(biocad_registry);
 let biocad_registry = open_registry("root", 123456);
 let uniprot = open.uniprot("J:\uniprot-all.xml\92938b97fc8aeaef3291c581f36554cc0301be29.xml");
 
-biocad_registry |> imports_uniprot(uniprot);
+biocad_registry |> imports_uniprot(uniprot, fast_check = TRUE);
