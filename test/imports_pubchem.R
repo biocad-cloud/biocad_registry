577391812a875f0081368f700eeae39b52484077
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-06 19:07:16 +0800

    Update imports_pubchem.R

--- test/imports_pubchem.R
+++ test/imports_pubchem.R
@@ -2,7 +2,7 @@ require(biocad_registry);
 
 imports "pubchem_kit" from "mzkit";
 
-let biocad_registry = open_registry("root", 123456);
+let biocad_registry = open_registry("root", 123456, host ="192.168.3.233");
 let pubchem = resolve_repository("J:\ossfs\pubchem\repo\pugViews");
 
 biocad_registry |> imports_pubchem (pubchem);
