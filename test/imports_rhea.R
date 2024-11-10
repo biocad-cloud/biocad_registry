01b7b2bd92a81badad662820f0088b7b0bab2d81
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-10 14:38:38 +0800

    Update imports_rhea.R

--- test/imports_rhea.R
+++ test/imports_rhea.R
@@ -2,7 +2,7 @@ require(biocad_registry);
 
 imports "rhea" from "annotationKit";
 
-let biocad_registry = open_registry("root", 123456);
+let biocad_registry = open_registry("root", 123456, host ="192.168.3.233");
 let rhea = rhea::open.rdf("J:\ossfs\rhea.rdf");
 
 rhea = rhea::reactions(rhea);
