fcbaff2995ac2af7c9f1c9fbb5377424677e016f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-10 18:38:05 +0800

    Update imports_uniprot.R

--- test/imports_uniprot.R
+++ test/imports_uniprot.R
@@ -1,6 +1,6 @@
 require(biocad_registry);
 
-let biocad_registry = open_registry("root", 123456);
+let biocad_registry = open_registry("root", 123456, host ="192.168.3.233");
 let uniprot = parseUniProt(readText(file.path(@dir,"uniprotkb_taxonomy_id_2_AND_model_organ_2024_08_02.xml")));
 
 biocad_registry |> imports_uniprot(uniprot);
