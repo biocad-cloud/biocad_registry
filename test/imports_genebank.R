f886d11c4fadc4ae4f2f6eddebe4cc638b1bf745
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-10 18:03:53 +0800

    Update imports_genebank.R

--- test/imports_genebank.R
+++ test/imports_genebank.R
@@ -2,7 +2,7 @@ require(biocad_registry);
 
 setwd(@dir);
 
-let biocad_registry = open_registry("root", 123456);
+let biocad_registry = open_registry("root", 123456, host ="192.168.3.233");
 let genebank =read.genbank("./Escherichia coli str. K-12 substr. MG1655.txt");
 
 print(unique(genebank |> featureKeys()));
