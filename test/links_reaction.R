364cc546f1e5d41d4a134b36e6082703fc1906de
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-12 23:34:47 +0800

    Update links_reaction.R

--- test/links_reaction.R
+++ test/links_reaction.R
@@ -1,5 +1,5 @@
 require(biocad_registry);
 
-let biocad_registry = open_registry("root", 123456);
+let biocad_registry = open_registry("root", 123456, host ="192.168.3.233");
 
 biocad_registry |> link_reaction_metabolites(); 
