60815ee06e9b19ce6cdcceb96a3752543610c6f6
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-21 19:14:48 +0800

    Update imports_uniprot_full.R

--- test/imports_uniprot_full.R
+++ test/imports_uniprot_full.R
@@ -1,6 +1,6 @@
 require(biocad_registry);
 
-let biocad_registry = open_registry("root", 123456);
+let biocad_registry = open_registry("root", 123456, host ="192.168.3.233");
 let uniprot = open.uniprot("J:\uniprot-all.xml\92938b97fc8aeaef3291c581f36554cc0301be29.xml");
 
 biocad_registry |> imports_uniprot(uniprot, fast_check = TRUE);
