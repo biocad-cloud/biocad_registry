bb76a24f1e3589132e627069aec53c741463998d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-21 15:43:05 +0800

    build mysql data models

---
+++ mysql/build.cmd
@@ -0,0 +1,8 @@
+REM @echo off
+
+set reflector="\graphQL\src\mysqli\App\Reflector.exe"
+set R_src="../src/mysql"
+
+%reflector% --reflects /sql ./cad_registry20241221.sql -o %R_src% /namespace biocad_registryModel --language visualbasic /split /auto_increment.disable
+
+REM pause
