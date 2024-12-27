7d63c6ec2aa6d06f60719935a9de38772af1f66b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-28 00:21:25 +0800

    export network graph in json list

--- test/export_test/export_reaction_network.R
+++ test/export_test/export_reaction_network.R
@@ -1,4 +1,8 @@
 require(biocad_registry);
+require(JSON);
 
 let biocad_registry = open_registry("root", 123456, host ="192.168.3.233");
 let graph = export_reactionLinks(biocad_registry); 
+let links = sapply(graph, l -> json_encode(l));
+
+writeLines(links, con = file.path(@dir, "reaction_graph.jsonl"));
