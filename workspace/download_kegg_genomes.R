bed594f46ff28ffc8ae35877f555c271432f02c4
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-03 14:56:00 +0800

    parse the genome source information

--- workspace/download_kegg_genomes.R
+++ workspace/download_kegg_genomes.R
@@ -1,8 +1,23 @@
 require(biocad_registry);
 
 imports "dbget" from "kegg_kit";
+imports "kegg_api" from "kegg_kit";
 
+# tools for download kegg bacterial genomes
 options(dbget.cache = @dir);
+
+let genomes = kegg_api::listing("organism");
+
+genomes <- lapply(genomes, str -> strsplit(str, "\t"));
+genomes <- data.frame(
+    row.names = names(genomes),
+    kegg_code = genomes@{1},
+    organism = genomes@{2},
+    kingdom = sapply(genomes@{3},str -> strsplit(str,";")[1]),
+    lineage = genomes@{3}
+);
+
+print(genomes);
 
 let org = dbget::show_organism("hsa");
 
