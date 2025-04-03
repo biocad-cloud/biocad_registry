bcc7157c7bb13ac966a85c6267c0dd462875eb41
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-03 16:49:17 +0800

    script for download genbank assembly file

---
+++ workspace/download_ncbi_genbank.R
@@ -0,0 +1,42 @@
+require(biocad_registry);
+
+imports "dbget" from "kegg_kit";
+imports "kegg_api" from "kegg_kit";
+
+# tools for download kegg bacterial genomes genbank annotation data
+options(dbget.cache = "D:\datapool");
+
+let genomes = kegg_api::listing("organism");
+
+genomes <- lapply(genomes, str -> strsplit(str, "\t"));
+genomes <- data.frame(
+    row.names = names(genomes),
+    kegg_code = genomes@{1},
+    organism = genomes@{2},
+    kingdom = sapply(genomes@{3},str -> strsplit(str,";")[1]),
+    lineage = genomes@{3}
+);
+
+print(genomes);
+print(unique(genomes$kingdom));
+
+let bacterial = genomes[genomes$kingdom == "Prokaryotes", ];
+let repo_dir= file.path(getOption("dbget.cache"),"ncbi_genbank");
+
+for(let entry in as.list(bacterial,byrow=TRUE)) {
+    let org = dbget::show_organism(entry$kegg_code);
+    let source = as.list(org)$DataSource;
+
+    source = source@text |> which(url -> (instr(url,"ncbi.nlm.nih.gov") > 1) && (instr(url, "assembly") > 1));
+    source = last(strsplit(source, "/"));
+    source = first(strsplit(source,".",fixed=TRUE));
+
+    str(source);
+
+    if (nchar(source) > 0) {
+        get_genbank(asm_id = source, repo_dir =  repo_dir);
+    }
+    
+    stop();
+}
+
