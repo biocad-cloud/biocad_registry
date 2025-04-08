6f133b6cc823f546a5c13c7144fc442c000aff03
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-08 15:06:08 +0800

    run package msbuild

---
+++ @export/data_imports.d.ts
@@ -0,0 +1,16 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "data_imports" from "biocad_registry";
+//
+// ref=biocad_registry.imports_api@biocad_registry, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+*/
+declare namespace data_imports {
+   /**
+   */
+   function imports_genbank(registry: object, genbank: object): any;
+   /**
+   */
+   function imports_taxonomy(registry: object, taxdump: object): any;
+}
---
+++ man/biocad_registry/data_imports/data_imports.md
@@ -0,0 +1,6 @@
+﻿# data_imports
+
+
+
++ [imports_genbank](data_imports/imports_genbank.1) 
++ [imports_taxonomy](data_imports/imports_taxonomy.1) 
