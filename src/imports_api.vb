7058d800811430315f10f6601b454ef92f6db998
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-08 14:08:18 +0800

    Create imports_api.vb

---
+++ src/imports_api.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank
+
+<Package("data_imports")>
+Module imports_api
+
+    <ExportAPI("imports_genbank")>
+    Public Function imports_genbank(registry As biocad_registry, genbank As GBFF.File) As Object
+        Call GenBankImports.ImportsData(registry, genbank)
+        Return Nothing
+    End Function
+
+End Module
