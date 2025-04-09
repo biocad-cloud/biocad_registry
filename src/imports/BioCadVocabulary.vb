3507ae11cb462f6de3f10d8377da956c60003ec5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-09 13:54:45 +0800

    term id

---
+++ src/imports/BioCadVocabulary.vb
@@ -0,0 +1,22 @@
+﻿Public Class BioCadVocabulary
+
+#Region "molecule type vocabulary"
+    Public ReadOnly Property gene_term As UInteger
+    Public ReadOnly Property rna_term As UInteger
+    Public ReadOnly Property protein_term As UInteger
+    Public ReadOnly Property metabolite_term As UInteger
+    Public ReadOnly Property complex_term As UInteger
+    Public ReadOnly Property dna_motif_term As UInteger
+    Public ReadOnly Property protein_motif_term As UInteger
+#End Region
+
+    Sub New(registry As biocad_registry)
+        gene_term = registry.getVocabulary("Nucleic Acid", "Molecule Type")
+        rna_term = registry.getVocabulary("RNA", "Molecule Type")
+        protein_term = registry.getVocabulary("Polypeptide", "Molecule Type")
+        metabolite_term = registry.getVocabulary("Metabolite", "Molecule Type")
+        complex_term = registry.getVocabulary("Complex", "Molecule Type")
+        dna_motif_term = registry.getVocabulary("Nucleotide Motif", "Molecule Type")
+        protein_motif_term = registry.getVocabulary("Protein Motif", "Molecule Type")
+    End Sub
+End Class
