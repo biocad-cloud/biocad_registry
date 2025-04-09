e8c6659ce9e3385f93a171c6c513d5e37e299eb0
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-09 15:10:49 +0800

    fetch data from genbank assembly

--- src/imports/GenBank.vb
+++ src/imports/GenBank.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
 Imports SMRUCC.genomics.Assembly.NCBI.GenBank
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank.GBFF.Keywords.FEATURES
 Imports SMRUCC.genomics.SequenceModel.FASTA
 
 Public Module GenBankImports
@@ -8,6 +9,26 @@ Public Module GenBankImports
     <Extension>
     Public Sub ImportsData(registry As biocad_registry, gb As GBFF.File)
         Dim vocabulary As New BioCadVocabulary(registry)
+        Dim ncbi_taxid = CInt(Val(gb.Taxon))
+        Dim cds = gb.Features.AsEnumerable _
+            .AsParallel _
+            .Where(Function(f) f.KeyName = "CDS") _
+            .ToDictionary(Function(f) f.Query(FeatureQualifiers.locus_tag),
+                          Function(f)
+                              Return f
+                          End Function)
+
+        For Each gene As Feature In gb.Features.AsEnumerable.Where(Function(f) f.KeyName = "gene")
+            Dim locus_tag As String = gene.Query(FeatureQualifiers.locus_tag)
+            Dim cds_feature = cds.TryGetValue(locus_tag)
+            Dim polypeptide As String = cds_feature.Query(FeatureQualifiers.translation)
+            Dim cds_id = cds_feature.Query(FeatureQualifiers.protein_id)
+            Dim func = cds_feature.Query(FeatureQualifiers.product)
+
+            If Not polypeptide Is Nothing Then
+
+            End If
+        Next
 
         Call registry.AddGenomics(gb, gb.Origin.ToFasta)
     End Sub
