413c944591dd6b07b96fd0e5e0bc65115f1b0d03
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-20 09:58:14 +0800

    Update imports_taxonomy.R

--- workspace/202504/imports_taxonomy.R
+++ workspace/202504/imports_taxonomy.R
@@ -3,7 +3,7 @@ require(biocad_registry);
 imports "taxonomy_kit" from "metagenomics_kit";
 imports "data_imports" from "biocad_registry";
 
-let biocad_registry = open_registry("root", 123456, host ="127.0.0.1");
+let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 let taxonomy_tree = taxonomy_kit::Ncbi.taxonomy_tree("D:\\datapool\\taxdump");
 
 data_imports::imports_taxonomy(biocad_registry, taxonomy_tree);
