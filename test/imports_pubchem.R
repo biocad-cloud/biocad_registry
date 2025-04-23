275b037033005cac76b924891ddaa0d8a0c49ff1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-23 23:17:14 +0800

    run msbuild

--- test/imports_pubchem.R
+++ test/imports_pubchem.R
@@ -2,8 +2,8 @@ require(biocad_registry);
 
 imports "pubchem_kit" from "mzkit";
 
-let biocad_registry = open_registry("root", 123456, host ="192.168.3.233");
+let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 let pubchem = resolve_repository("J:\ossfs\pubchem\repo\pugViews");
 
-# biocad_registry |> imports_pubchem (pubchem);
-biocad_registry |> imports_odor(pubchem,fast_check=TRUE); 
+biocad_registry |> imports_pubchem (pubchem);
+# biocad_registry |> imports_odor(pubchem,fast_check=TRUE); 
