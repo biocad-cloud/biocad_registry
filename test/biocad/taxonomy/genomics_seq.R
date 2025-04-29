bdcff259c69838172984949818743f62e60cbd15
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-30 01:19:29 +0800

    Update genomics_seq.R

--- test/biocad/taxonomy/genomics_seq.R
+++ test/biocad/taxonomy/genomics_seq.R
@@ -1,6 +1,6 @@
 require(biocad_registry);
 
 let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
-let nucleotide = biocad_registry |> export_genomics_fasta(parent_taxname = "Alicyclobacillaceae");
+let nucleotide = biocad_registry |> export_genomics_fasta(parent_taxname = "Bacillales");
 
 print(nucleotide);
