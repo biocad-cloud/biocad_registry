49b6fd88622cbfacb4dc06ccd48c95f93a060a9d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-30 00:22:53 +0800

    add xml comment docs

--- test/biocad/taxonomy/taxinfo.R
+++ test/biocad/taxonomy/taxinfo.R
@@ -6,3 +6,6 @@ str(as.list(biocad_registry |> get_taxinfo("Pholiotina communis")));
 
 
 print(as.data.frame(biocad_registry |> find_taxinfo("Pholiotina")));
+
+
+print(biocad_registry |> taxonomy_lineage("Pholiotina vexans") |> toString());
