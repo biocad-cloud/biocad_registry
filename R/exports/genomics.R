2956755f2c31ffa1689ca1711039a06d9dcd28dd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-30 00:49:17 +0800

    Update genomics.R

--- R/exports/genomics.R
+++ R/exports/genomics.R
@@ -1,3 +1,4 @@
-const export_genomics_fasta = function(biocad_registry) {
+const export_genomics_fasta = function(biocad_registry, parent_taxname) {
+    let taxnode = biocad_registry |> get_taxinfo(parent_taxname);
     
 }
