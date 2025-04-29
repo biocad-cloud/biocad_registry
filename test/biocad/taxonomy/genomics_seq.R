2f7d7358d81dccd16cdefa07b8dfbfb022571148
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-30 01:36:24 +0800

    write export fasta sequence success

--- test/biocad/taxonomy/genomics_seq.R
+++ test/biocad/taxonomy/genomics_seq.R
@@ -3,4 +3,5 @@ require(biocad_registry);
 let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 let nucleotide = biocad_registry |> export_genomics_fasta(parent_taxname = "Bacillales", fasta = TRUE);
 
-print(nucleotide);
+# print(nucleotide);
+write.fasta(nucleotide, file = file.path(@dir, "Bacillales_genomics.fasta"));
