225d70561c7e654c2509e5368a79f5f2477cdff0
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-29 23:38:49 +0800

    Update taxinfo.R

--- test/biocad/taxonomy/taxinfo.R
+++ test/biocad/taxonomy/taxinfo.R
@@ -2,3 +2,7 @@ require(biocad_registry);
 
 let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 
+str(as.list(biocad_registry |> get_taxinfo("Pholiotina communis")));
+
+
+print(as.data.frame(biocad_registry |> find_taxinfo("Pholiotina")));
