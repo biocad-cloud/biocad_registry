6ef4db9d429c7a2b7cd46fe8cff3ba56af327e44
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-10 17:39:08 +0800

    find molecule

--- src/imports/BatchDataCommit/PubChemImports.vb
+++ src/imports/BatchDataCommit/PubChemImports.vb
@@ -1,7 +1,10 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports BioNovoGene.BioDeep.Chemistry.MetaLib.CrossReference
 Imports BioNovoGene.BioDeep.Chemistry.MetaLib.Models
 Imports BioNovoGene.BioDeep.Chemistry.NCBI.PubChem
 Imports BioNovoGene.BioDeep.Chemoinformatics.Formula
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Linq
 Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
 
 Public Module PubChemImports
@@ -33,7 +36,7 @@ Public Module PubChemImports
                 Call trans.add(
                     field("xref_id") = $"PubChem:{meta.ID}",
                     field("name") = meta.name,
-                    field("mass") = mass,
+                    field("mass") = If(mass < 0, 0, mass),
                     field("type") = terms.metabolite_term,
                     field("formula") = meta.formula,
                     field("parent") = 0,
@@ -50,6 +53,61 @@ Public Module PubChemImports
     Private Function findMolecule(registry As biocad_registry, meta As MetaLib) As biocad_registryModel.molecule
         Dim cid As String = $"PubChem:{meta.ID}"
 
+        ' find molecule table via xref_id directly at first
+        Dim q = registry.molecule.where(field("xref_id") = cid).find(Of biocad_registryModel.molecule)
+
+        If Not q Is Nothing Then
+            Return q
+        End If
+
+        ' find molecule via db_xrefs
+        Dim filter As FieldAssert = FieldAssert.empty
+        Dim xrefs As NamedValue(Of String)() = meta.xref.PopulateXrefs(True, True).ToArray
+        Dim mass As Double = FormulaScanner.EvaluateExactMass(meta.formula)
+
+        For Each xref In xrefs
+            filter = filter Or (field("db_key") = registry.vocabulary_terms.GetDatabaseKey(xref.Name) And field("xref") = xref.Value)
+        Next
+
+        q = registry.molecule _
+            .left_join("db_xrefs") _
+            .on(field("`db_xrefs`.obj_id") = field("`molecule`.id")) _
+            .where(field("mass").between(mass - 1, mass + 1), filter) _
+            .find(Of biocad_registryModel.molecule)("`molecule`.*")
+
+        If Not q Is Nothing Then
+            Return q
+        End If
+
+        ' find molecule table via name
+        q = registry.molecule _
+            .where(field("mass").between(mass - 1, mass + 1),
+                   field("name") = meta.name) _
+            .find(Of biocad_registryModel.molecule)
+
+        If Not q Is Nothing Then
+            Return q
+        End If
+
+        Dim hashset As String() = {meta.name, meta.IUPACName} _
+            .JoinIterates(meta.synonym) _
+            .Where(Function(s) Not s.StringEmpty(, True)) _
+            .Select(Function(s) s.ToLower.MD5) _
+            .ToArray
+
+        ' find molecule via the synonym
+        q = registry.molecule _
+            .left_join("synonym") _
+            .on(field("`synonym`.obj_id") = field("`molecule`.id")) _
+            .where(field("mass").between(mass - 1, mass + 1),
+                   field("hashcode").in(hashset)) _
+            .find(Of biocad_registryModel.molecule)("`molecule`.*")
+
+        If Not q Is Nothing Then
+            Return q
+        End If
+
+        Return Nothing
     End Function
 
 End Module
