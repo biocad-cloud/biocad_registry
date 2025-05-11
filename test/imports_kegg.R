c19a240839deff3ea66ebb0904e51d445459f40c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-12 07:58:07 +0800

    Update imports_kegg.R

--- test/imports_kegg.R
+++ test/imports_kegg.R
@@ -1,7 +1,7 @@
 require(biocad_registry);
 require(GCModeller);
 
-let biocad_registry = open_registry("root", 123456, host ="192.168.3.233");
+let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 let kegg = GCModeller::kegg_compounds(reference_set=FALSE);
 # let reactions = GCModeller::kegg_reactions();
 
