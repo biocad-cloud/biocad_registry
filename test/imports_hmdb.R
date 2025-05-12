018f9679a802af4126376ddb7169a8b850138b3b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-12 13:32:35 +0800

    Create imports_hmdb.R

---
+++ test/imports_hmdb.R
@@ -0,0 +1,12 @@
+require(biocad_registry);
+
+imports "hmdb_kit" from "mzkit";
+imports "data_imports" from "biocad_registry";
+
+let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
+
+biocad_registry |> imports_metab_repo(read.hmdb(
+    xml = "", 
+    convert_std = TRUE, 
+    tqdm = FALSE
+));
