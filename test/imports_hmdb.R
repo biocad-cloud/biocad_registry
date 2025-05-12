faac2be1bf63a6a61ae50a88a470d689c25d10e5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-12 13:38:51 +0800

    Update imports_hmdb.R

--- test/imports_hmdb.R
+++ test/imports_hmdb.R
@@ -6,7 +6,7 @@ imports "data_imports" from "biocad_registry";
 let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 
 biocad_registry |> imports_metab_repo(read.hmdb(
-    xml = "", 
+    xml = "U:\hmdb\hmdb_metabolites.xml", 
     convert_std = TRUE, 
     tqdm = FALSE
 ));
