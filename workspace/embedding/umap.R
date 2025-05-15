b663aa261978ee21d3e641abbb044d3214bac5f1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-15 22:59:59 +0800

    make umap embedding

---
+++ workspace/embedding/umap.R
@@ -0,0 +1,10 @@
+require(umap);
+
+setwd(@dir);
+
+let fingerprint = read.csv("./nt_fingerprints.csv", row.names = 1, check.names = FALSE);
+let embedding = umap(fingerprint, dimension = 9, numberOfNeighbors = 64);
+
+embedding = as.data.frame(embedding$umap, labels = embedding$labels);
+
+write.csv(embedding, file = "./nt_embedding.csv");
