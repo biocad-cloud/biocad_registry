8e58f53c707bccd4404bff8a31478983ec78eff7
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-15 23:11:57 +0800

    Update umap.R

--- workspace/embedding/umap.R
+++ workspace/embedding/umap.R
@@ -3,7 +3,7 @@ require(umap);
 setwd(@dir);
 
 let fingerprint = read.csv("./nt_fingerprints.csv", row.names = 1, check.names = FALSE);
-let embedding = umap(fingerprint, dimension = 9, numberOfNeighbors = 64);
+let embedding = umap(fingerprint, dimension = 9, numberOfNeighbors = 128);
 
 embedding = as.data.frame(embedding$umap, labels = embedding$labels);
 
