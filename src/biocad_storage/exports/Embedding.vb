37b2673ad95921e142b1573ae64d05220004151d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-15 22:05:01 +0800

    extract of the matrix data

--- src/biocad_storage/exports/Embedding.vb
+++ src/biocad_storage/exports/Embedding.vb
@@ -1,5 +1,7 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Net.Http
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
 
 Public Module Embedding
 
@@ -16,6 +18,27 @@ Public Module Embedding
         Dim offset As UInteger
         Dim page_data As biocad_registryModel.genomics()
 
+        For page As Integer = 1 To Integer.MaxValue
+            offset = (page - 1) * page_size
+            page_data = registry.genomics _
+                .where(match("def").against("+complete -plasmid", booleanMode:=True), Not field("fingerprint").is_nothing) _
+                .limit(offset, page_size) _
+                .select(Of biocad_registryModel.genomics)
+
+            If page_data.IsNullOrEmpty Then
+                Exit For
+            End If
+
+            For Each genome As biocad_registryModel.genomics In From nt As biocad_registryModel.genomics
+                                                                In page_data
+                                                                Where nt.fingerprint.Length > 0
+
+                Dim fingerprint As Byte() = genome.fingerprint.UnGzipBase64.ToArray
+                Dim v As Double() = fingerprint.Select(Function(b) CDbl(b)).ToArray
+
+                Yield New NamedCollection(Of Double)(genome.id.ToString, v, genome.biom_string)
+            Next
+        Next
     End Function
 
 End Module
