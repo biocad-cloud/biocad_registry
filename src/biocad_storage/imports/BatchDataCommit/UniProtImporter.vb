0ee151624f755aaff2eec604dbae4adbcfefa9e8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-25 22:14:13 +0800

    load unorpto in pahe data

--- src/biocad_storage/imports/BatchDataCommit/UniProtImporter.vb
+++ src/biocad_storage/imports/BatchDataCommit/UniProtImporter.vb
@@ -1,3 +1,28 @@
-﻿Public Class Class1
+﻿Imports SMRUCC.genomics.Assembly.Uniprot.XML
+
+Public Class UniProtImporter
+
+End Class
+
+Public Class UniProtPageLoader
+
+    ReadOnly repo As String
+
+    Sub New(repo As String)
+        Me.repo = repo
+    End Sub
+
+    Public Iterator Function LoadPageData() As IEnumerable(Of entry())
+        Dim tmp As New List(Of entry)
+
+        For Each prot As entry In UniProtXML.EnumerateEntries({repo}, ignoreError:=True, tqdm:=True)
+            Call tmp.Add(prot)
+
+            If tmp.Count > 3000 Then
+                Yield tmp.ToArray
+                Call tmp.Clear()
+            End If
+        Next
+    End Function
 
 End Class
