6eb8ed146519d40e52f625c32cc5d6f327721338
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-11 19:43:57 +0800

    config imports uniprot of bacterial data

--- test/imports_refmet.R
+++ test/imports_refmet.R
@@ -4,4 +4,4 @@ imports "data_imports" from "biocad_registry";
 
 let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 
-biocad_registry |> imports_refmet("G:\metaboanalyst\data\refmet.csv");
+biocad_registry |> imports_refmet("U:\uniprot_sprot_bacteria.xml");
