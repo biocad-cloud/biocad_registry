69204a159e171e1b1bb4d6852ebfd9c97899a03c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-14 02:12:44 +0800

    config imports go

--- src/fixTool/importsData.vb
+++ src/fixTool/importsData.vb
@@ -3,7 +3,8 @@
 Module importsData
 
     Sub ImportsChebi()
-        Dim workflow As New ChEBIOntologyImports(Program.registry, "G:\biocad_registry\data\chebi_lite.obo")
+        Dim workflow As New GeneOntologyImports(Program.registry, "G:\biocad_registry\data\go.obo")
+        ' Dim workflow As New ChEBIOntologyImports(Program.registry, "G:\biocad_registry\data\chebi_lite.obo")
         '  workflow.RunImports()
         workflow.ImportsOntology()
 
