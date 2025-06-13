d6b69e4fe89e5d934a9636809f1b1389059e4a43
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-13 22:14:32 +0800

    Update importsData.vb

--- src/fixTool/importsData.vb
+++ src/fixTool/importsData.vb
@@ -5,5 +5,8 @@ Module importsData
     Sub ImportsChebi()
         Dim workflow As New ChEBIOntologyImports(Program.registry, "G:\biocad_registry\data\chebi_lite.obo")
         workflow.RunImports()
+
+
+        Pause()
     End Sub
 End Module
