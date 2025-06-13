8ee75d433fe94ff68cdc1611fab5d472dbcd006b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-14 02:42:02 +0800

    save all

--- R/links/link_reaction_metabolites.R
+++ R/links/link_reaction_metabolites.R
@@ -1,7 +1,7 @@
 #' Set molecule id link
 #' 
 const link_reaction_metabolites = function(biocad_registry) {
-    let page_size = 3000;
+    let page_size = 20000;
     let molecule = biocad_registry |> table("molecule");
     let term_metabolite = metabolite_term(biocad_registry); 
 
@@ -19,26 +19,24 @@ const link_reaction_metabolites = function(biocad_registry) {
         if (length(graph_links) == 0) {
             break;
         } else {
-            # print(graph_links);
-        }
+            for(let link in tqdm(as.list(graph_links, byrow = TRUE))) {
+                if (link$molecule_id == 0) {
+                    let mol = molecule 
+                        |> left_join("db_xrefs") 
+                        |> on(db_xrefs.obj_id = molecule.id) 
+                        |> where(molecule.type = term_metabolite, 
+                            xref = link$db_xref) 
+                        |> find("molecule.id")
+                        ;
 
-        for(let link in tqdm(as.list(graph_links, byrow = TRUE))) {
-            if (link$molecule_id == 0) {
-                let mol = molecule 
-                    |> left_join("db_xrefs") 
-                    |> on(db_xrefs.obj_id = molecule.id) 
-                    |> where(molecule.type = term_metabolite, 
-                        xref = link$db_xref) 
-                    |> find("molecule.id")
-                    ;
-
-                if (!is.null(mol$id)) {
-                    biocad_registry 
-                    |> table("reaction_graph") 
-                    |> where(id = link$id)
-                    |> limit(1)
-                    |> save(molecule_id = mol$id)
-                    ;
+                    if (!is.null(mol$id)) {
+                        biocad_registry 
+                        |> table("reaction_graph") 
+                        |> where(id = link$id)
+                        |> limit(1)
+                        |> save(molecule_id = mol$id)
+                        ;
+                    }
                 }
             }
         }
