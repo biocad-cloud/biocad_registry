e0b1a8e4f8e41e124b9b38c899852e619487e1a0
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-13 14:25:05 +0800

    embedding not so ggod

--- workspace/embedding/plot_enzyme.R
+++ workspace/embedding/plot_enzyme.R
@@ -2,9 +2,11 @@ require(ggplot);
 
 options(strict = FALSE);
 
+let data = read.csv(`${@dir}/enzyme_embedding.csv`, row.names = 1);
+
 bitmap(file = `${@dir}/UMAP3d.png`, size = [4800, 3600]) {
     
-	let data = read.csv(`${@dir}/enzyme_embedding.csv`, row.names = 1);
+	
 	# data[, "class"] = `class_${rownames(data)}`;
 	
 	# colorset = {
@@ -23,11 +25,27 @@ bitmap(file = `${@dir}/UMAP3d.png`, size = [4800, 3600]) {
 	print(unique(data[, "class"]));
 	
 	# create ggplot layers and tweaks via ggplot style options
-	ggplot(data, aes(x = "dimension_1", y = "dimension_2", z = "dimension_3"), padding = "padding:250px 500px 100px 100px;")
+	ggplot(data, aes(x = "dimension_1", y = "dimension_2"#, z = "dimension_3"
+	), padding = "padding:250px 500px 100px 100px;")
 	+ geom_point(aes(color = "class"), color = "paper", shape = "circle", size = 20)
-	+ view_camera(angle = [31.5,65,125], fov = 10000)
+	# + view_camera(angle = [31.5,65,125], fov = 10000)
 	+ ggtitle("Enzyme Embedding")
 	+ theme_default()
 	;
 
+}
+
+for(let id in unique(data$class)) {
+	let subdata = data[data$class == id, ];
+
+	bitmap(file = relative_work(`${id}.png`), size = [4800,3600]) {
+		# create ggplot layers and tweaks via ggplot style options
+		ggplot(subdata, aes(x = "dimension_1", y = "dimension_2", z = "dimension_3"
+		), padding = "padding:250px 500px 100px 100px;")
+		+ geom_point(aes(color = "class"), color = "paper", shape = "circle", size = 20)
+		+ view_camera(angle = [31.5,65,125], fov = 10000)
+		+ ggtitle("Enzyme Embedding")
+		+ theme_default()
+		;
+	}
 }
