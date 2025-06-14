54c6c32889cd7f163b57bffbe89fcd33612275de
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-14 12:13:20 +0800

    run hmdb imports

--- src/fixTool/importsData.vb
+++ src/fixTool/importsData.vb
@@ -3,10 +3,12 @@
 Module importsData
 
     Sub ImportsChebi()
-        Dim workflow As New GeneOntologyImports(Program.registry, "G:\biocad_registry\data\go.obo")
+        ' Dim workflow As New GeneOntologyImports(Program.registry, "G:\biocad_registry\data\go.obo")
         ' Dim workflow As New ChEBIOntologyImports(Program.registry, "G:\biocad_registry\data\chebi_lite.obo")
         '  workflow.RunImports()
-        workflow.ImportsOntology()
+        ' workflow.ImportsOntology()
+        Dim hmdb As New HMDBImports(Program.registry, "U:\hmdb\hmdb_metabolites.xml")
+        hmdb.Imports()
 
         Pause()
     End Sub
