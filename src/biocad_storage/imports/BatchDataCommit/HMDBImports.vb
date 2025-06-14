3fcb92e0e44daf53d3b8fe524d590dfa4070e11b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-14 11:01:33 +0800

    do metabolite imports commit

---
+++ src/biocad_storage/imports/BatchDataCommit/HMDBImports.vb
@@ -0,0 +1,32 @@
+﻿Imports BioNovoGene.BioDeep.Chemistry.MetaLib.Models
+Imports BioNovoGene.BioDeep.Chemistry.TMIC
+Imports BioNovoGene.BioDeep.Chemistry.TMIC.HMDB
+Imports BioNovoGene.BioDeep.Chemoinformatics.Formula
+Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar.Tqdm
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+
+Public Class HMDBImports
+
+    ReadOnly registry As biocad_registry
+    ReadOnly hmdbfile As String
+    ReadOnly terms As BioCadVocabulary
+
+    Sub New(registry As biocad_registry, hmdb As String)
+        Me.registry = registry
+        Me.hmdbfile = hmdb
+        Me.terms = registry.vocabulary_terms
+    End Sub
+
+    Public Sub [Imports]()
+        For Each page As HMDB.metabolite() In HMDB.metabolite.Load(hmdbfile).SplitIterator(100)
+            Call [Imports](page)
+        Next
+    End Sub
+
+    Private Sub [Imports](page As HMDB.metabolite())
+        Dim metadata As MetaLib() = page.ConvertInternal.ToArray
+
+
+    End Sub
+
+End Class
---
+++ src/biocad_storage/imports/BatchDataCommit/MetaboliteCommit.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Runtime.CompilerServices
+Imports BioNovoGene.BioDeep.Chemistry.MetaLib.Models
+Imports BioNovoGene.BioDeep.Chemoinformatics.Formula
+Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar.Tqdm
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+
+Module MetaboliteCommit
+
+    <Extension>
+    Public Sub CommitMetabolites(Of T As MetaInfo)(metabolites As IEnumerable(Of T), registry As biocad_registry)
+        Dim terms As BioCadVocabulary = registry.vocabulary_terms
+
+        For Each meta As T In TqdmWrapper.Wrap(metabolites.ToArray)
+            Dim mol As biocad_registryModel.molecule = registry.findMolecule(meta, Function(a) a.ID)
+
+            If mol Is Nothing Then
+                Dim mass As Double = FormulaScanner.EvaluateExactMass(meta.formula)
+                Dim molData = {
+                    field("xref_id") = meta.ID,
+                    field("name") = meta.name,
+                    field("mass") = If(mass < 0, 0, mass),
+                    field("type") = terms.metabolite_term,
+                    field("formula") = meta.formula,
+                    field("parent") = 0,
+                    field("tax_id") = 0,
+                    field("note") = meta.description
+                }
+
+                ' add new 
+                Call registry.molecule.add(molData)
+            End If
+        Next
+    End Sub
+End Module
