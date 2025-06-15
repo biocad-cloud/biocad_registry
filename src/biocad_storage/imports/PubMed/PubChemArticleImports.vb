31c8c5e91a6ba255f15b397e70bfa2f6ea9be369
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-15 13:46:48 +0800

    update database schema

---
+++ src/biocad_storage/imports/PubMed/PubChemArticleImports.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar.Tqdm
+Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.NCBI.PubMed
+
+Public Class PubChemArticleImports
+
+    ReadOnly registry As biocad_registry
+    ReadOnly terms As BioCadVocabulary
+
+    Sub New(registry As biocad_registry)
+        Me.terms = registry.vocabulary_terms
+        Me.registry = registry
+    End Sub
+
+    Public Sub MakeImports(articles As PubChemTextJSON(), topic As String)
+        For Each article As PubChemTextJSON In TqdmWrapper.Wrap(articles)
+
+        Next
+    End Sub
+End Class
