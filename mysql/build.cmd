28bd28b5cb3245d53c9b5608316044a5194b2554
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-15 13:48:06 +0800

    update object schema mampping source

--- mysql/build.cmd
+++ mysql/build.cmd
@@ -3,6 +3,6 @@ REM @echo off
 set reflector="\graphQL\src\mysqli\App\Reflector.exe"
 set R_src="../src/biocad_storage/mysql"
 
-%reflector% --reflects /sql ./cad_registry20241221.sql -o %R_src% /namespace biocad_registryModel --language visualbasic /split /auto_increment.disable
+%reflector% --reflects /sql ./cad_registry20250615.sql -o %R_src% /namespace biocad_registryModel --language visualbasic /split /auto_increment.disable
 
 REM pause
