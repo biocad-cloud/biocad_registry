4c9f8fde9d92a63f5bf46088deb8d0bff26d6f4b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-20 21:25:21 +0800

    Update imports_sabiork.R

--- test/imports_sabiork.R
+++ test/imports_sabiork.R
@@ -1,6 +1,6 @@
 require(biocad_registry);
 
-let biocad_registry = open_registry("root", 123456, host ="192.168.3.233");
+let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 let repo = list.files("J:\2022_nar\sabio-rk\.cache", pattern = "*.xml", recursive =TRUE);
 
 print(repo);
