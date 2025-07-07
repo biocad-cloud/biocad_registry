afbc3d64a2113347537fd455c383dca57d7f3317
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-07-07 23:00:10 +0800

    create unique hash code of the reaction models

--- src/biocad_storage/reactions.vb
+++ src/biocad_storage/reactions.vb
@@ -1,3 +1,27 @@
-﻿Public Module reactions
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar.Tqdm
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
 
+Public Module reactions
+
+    Public Sub BuildUniqeHashCode(registry As biocad_registry, Optional page_size As Integer = 1000)
+        Dim page_data As biocad_registryModel.reaction()
+        Dim left_key = registry.getVocabulary("substrate", "Compound Role")
+        Dim right_key = registry.getVocabulary("product", "Compound Role")
+
+        For i As Integer = 0 To 100000
+            page_data = registry.reaction _
+                .limit(i * page_size, page_size) _
+                .select(Of biocad_registryModel.reaction)
+
+            For Each rxn As biocad_registryModel.reaction In TqdmWrapper.Wrap(page_data)
+                Dim left = registry.reaction_graph.where(field("reaction") = rxn.id, field("role") = left_key).project(Of UInteger)("molecule_id")
+                Dim right = registry.reaction_graph.where(field("reaction") = rxn.id, field("role") = right_key).project(Of UInteger)("molecule_id")
+                Dim left_hash = left.OrderBy(Function(id) id).JoinBy("+").MD5
+                Dim right_hash = right.OrderBy(Function(id) id).JoinBy("+").MD5
+                Dim uniqecode = left_hash & right_hash
+
+                registry.reaction.where(field("id") = rxn.id).save(field("hashcode") = uniqecode)
+            Next
+        Next
+    End Sub
 End Module
