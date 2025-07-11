049b9be769a7ac9b951325d4593f0bb800a590d9
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-07-12 01:09:54 +0800

    make bugs fixed for the unique hash calculation of the reaction network data

--- src/biocad_storage/reactions.vb
+++ src/biocad_storage/reactions.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar.Tqdm
+Imports Microsoft.VisualBasic.Linq
 Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
 
 Public Module reactions
@@ -18,11 +19,14 @@ Public Module reactions
                 .select(Of biocad_registryModel.reaction)
 
             For Each rxn As biocad_registryModel.reaction In TqdmWrapper.Wrap(page_data)
-                Dim left = registry.reaction_graph.where(field("reaction") = rxn.id, field("role") = left_key).project(Of UInteger)("molecule_id")
-                Dim right = registry.reaction_graph.where(field("reaction") = rxn.id, field("role") = right_key).project(Of UInteger)("molecule_id")
-                Dim left_hash = left.OrderBy(Function(id) id).JoinBy("+").MD5
-                Dim right_hash = right.OrderBy(Function(id) id).JoinBy("+").MD5
-                Dim enzymes = registry.regulation_graph.where(field("reaction_id") = rxn.id, field("role") = enzyme_role).select(Of biocad_registryModel.regulation_graph)
+                Dim left = registry.reaction_graph.where(field("reaction") = rxn.id, field("role") = left_key).select(Of biocad_registryModel.reaction_graph)
+                Dim right = registry.reaction_graph.where(field("reaction") = rxn.id, field("role") = right_key).select(Of biocad_registryModel.reaction_graph)
+                Dim left_hash = CheckSum(left)
+                Dim right_hash = CheckSum(right)
+                Dim enzymes = registry.regulation_graph _
+                    .where(field("reaction_id") = rxn.id,
+                           field("role") = enzyme_role) _
+                    .select(Of biocad_registryModel.regulation_graph)
                 Dim uniqecode As String
 
                 ' clear unique hashcode of current object
@@ -32,14 +36,26 @@ Public Module reactions
                     uniqecode = {left_hash, right_hash}.OrderBy(Function(s) s).JoinBy("").MD5
                     registry.hashcode.add(field("type_id") = reaction_type, field("obj_id") = rxn.id, field("hashcode") = uniqecode)
                 Else
-                    For Each enzyme In enzymes
+                    For Each enzyme As biocad_registryModel.regulation_graph In enzymes
                         uniqecode = {left_hash, right_hash}.OrderBy(Function(s) s).JoinBy(enzyme.term).MD5
                         registry.hashcode.add(field("type_id") = reaction_type, field("obj_id") = rxn.id, field("hashcode") = uniqecode)
                     Next
                 End If
 
-                registry.reaction.where(field("id") = rxn.id).save(field("hashcode") = {left_hash, right_hash}.OrderBy(Function(s) s).JoinBy("").MD5)
+                Call registry.reaction _
+                    .where(field("id") = rxn.id) _
+                    .save(field("hashcode") = New String() {left_hash, right_hash} _
+                                                .OrderBy(Function(s) s) _
+                                                .JoinBy("") _
+                                                .MD5)
             Next
         Next
     End Sub
+
+    Private Function CheckSum(compounds As biocad_registryModel.reaction_graph()) As String
+        Dim sort = compounds.SafeQuery.OrderBy(Function(a) a.molecule_id).ToArray
+        Dim data = sort.Select(Function(a) If(a.molecule_id = 0, a.db_xref, a.molecule_id.ToString)).JoinBy("+")
+        Dim checksum_result = data.MD5
+        Return checksum_result
+    End Function
 End Module
