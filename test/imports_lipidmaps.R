9639dd290cc7a9908de2dab7a71f31b7106940d4
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-06 21:05:54 +0800

    config script for imports lipidmaps

--- test/imports_lipidmaps.R
+++ test/imports_lipidmaps.R
@@ -3,3 +3,8 @@ require(biocad_registry);
 imports "massbank" from "mzkit";
 imports "data_imports" from "biocad_registry";
 
+let dataset = read.SDF(file = "G:\biocad_registry\data\lipidmaps.sdf", lazy = FALSE) |> lipid_metabolites();
+let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
+
+biocad_registry |> imports_metab_repo(dataset);
+
