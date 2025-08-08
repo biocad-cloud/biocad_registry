30098db81d5b781e21b18a20bb72b62fd1ad9d6a
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-08 10:13:16 +0800

    format export code

--- src/fixTool/exportData.vb
+++ src/fixTool/exportData.vb
@@ -16,20 +16,32 @@ Module exportData
             "Synthetic steroids", "antibiotic", "carcinogen", "industrial pollutants", "Drug metabolites"
         }
 
-        Dim plant = topics_plant.Select(Function(tag)
-                                            Return Program.registry.ExportTagData(tag).Select(Function(id) (id, tag, "plant"))
-                                        End Function).IteratesALL.Where(Function(a) a.id.exact_mass > 50 AndAlso a.id.exact_mass < 1200).ToArray
-        Dim all = plant.GroupBy(Function(r) r.id.id) _
+        Dim plant = topics_plant _
+            .Select(Function(tag)
+                        Return Program.registry.ExportTagData(tag).Select(Function(id) (id, tag, "plant"))
+                    End Function) _
+            .IteratesALL _
+            .Where(Function(a) a.id.exact_mass > 50 AndAlso a.id.exact_mass < 1200) _
+            .ToArray
+        Dim notplant = excludes _
+            .Select(Function(tag)
+                        Return Program.registry.ExportTagData(tag).Select(Function(id) (id, tag, "not plant"))
+                    End Function) _
+            .IteratesALL _
+            .Where(Function(a) a.id.exact_mass > 50 AndAlso a.id.exact_mass < 1200) _
+            .ToArray
+        Dim all = plant.JoinIterates(notplant) _
+            .GroupBy(Function(r) r.id.id) _
             .Select(Function(r)
                         Dim name = r.First.id.name
                         Dim id = r.First.id.id
                         Dim plants = r.Where(Function(a) a.Item3 = "plant").Select(Function(a) a.tag).Distinct.ToArray
                         Dim not_plants = r.Where(Function(a) a.Item3 <> "plant").Select(Function(a) a.tag).Distinct.ToArray
 
-                        Return (id, name, plants, not_plants)
+                        Return (id, name, score:=plants.Length / (not_plants.Length + 1), plants, not_plants)
                     End Function) _
             .OrderByDescending(Function(a)
-                                   Return a.plants.TryCount
+                                   Return a.score
                                End Function) _
             .Take(20000) _
             .ToArray
