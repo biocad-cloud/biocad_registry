00fa7dcf3e98f68b5d5222f5d2965aa73cce42ae
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-11 03:59:47 +0800

    batch edit of the topics

--- src/RegistryTool/Tables/FormBatchEditor.Designer.vb
+++ src/RegistryTool/Tables/FormBatchEditor.Designer.vb
@@ -22,17 +22,31 @@ Partial Class FormBatchEditor
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.OpenInEditorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RemoveFromListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox1.SuspendLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.Button1)
         Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Bottom
         Me.GroupBox1.Location = New System.Drawing.Point(0, 313)
         Me.GroupBox1.Name = "GroupBox1"
@@ -41,14 +55,55 @@ Partial Class FormBatchEditor
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Operator"
         '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(369, 42)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Remove"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Tags"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(84, 42)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(161, 20)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(288, 42)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'DataGridView1
         '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.Gainsboro
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4})
+        Me.DataGridView1.ContextMenuStrip = Me.ContextMenuStrip1
         Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
         Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
         Me.DataGridView1.RowTemplate.Height = 23
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
         Me.DataGridView1.Size = New System.Drawing.Size(800, 313)
         Me.DataGridView1.TabIndex = 1
         '
@@ -77,6 +132,24 @@ Partial Class FormBatchEditor
         Me.Column4.Name = "Column4"
         Me.Column4.ReadOnly = True
         '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenInEditorToolStripMenuItem, Me.RemoveFromListToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(170, 48)
+        '
+        'OpenInEditorToolStripMenuItem
+        '
+        Me.OpenInEditorToolStripMenuItem.Name = "OpenInEditorToolStripMenuItem"
+        Me.OpenInEditorToolStripMenuItem.Size = New System.Drawing.Size(169, 22)
+        Me.OpenInEditorToolStripMenuItem.Text = "Open In Editor"
+        '
+        'RemoveFromListToolStripMenuItem
+        '
+        Me.RemoveFromListToolStripMenuItem.Name = "RemoveFromListToolStripMenuItem"
+        Me.RemoveFromListToolStripMenuItem.Size = New System.Drawing.Size(169, 22)
+        Me.RemoveFromListToolStripMenuItem.Text = "Remove From List"
+        '
         'FormBatchEditor
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
@@ -86,7 +159,10 @@ Partial Class FormBatchEditor
         Me.Controls.Add(Me.GroupBox1)
         Me.Name = "FormBatchEditor"
         Me.Text = "Batch Editor"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -97,4 +173,11 @@ Partial Class FormBatchEditor
     Friend WithEvents Column2 As DataGridViewTextBoxColumn
     Friend WithEvents Column3 As DataGridViewTextBoxColumn
     Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents OpenInEditorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RemoveFromListToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button1 As Button
 End Class
--- src/RegistryTool/Tables/FormBatchEditor.vb
+++ src/RegistryTool/Tables/FormBatchEditor.vb
@@ -1,9 +1,15 @@
-﻿Public Class FormBatchEditor
+﻿Imports RegistryTool.My
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+Imports biocad_storage
+
+Public Class FormBatchEditor
 
     Dim list As MoleculeBatchView()
 
     Public Sub LoadFromIDSet(ids As IEnumerable(Of String))
         list = MoleculeBatchView.FromIdSet(ids.Select(Function(id) UInteger.Parse(id.Match("\d+"))))
+
+        Call DataGridView1.Rows.Clear()
 
         For Each mol As MoleculeBatchView In list
             Dim offset = DataGridView1.Rows.Add(mol.id, mol.name, mol.tags, mol.note)
@@ -11,5 +17,91 @@
         Next
 
         DataGridView1.CommitEdit(DataGridViewDataErrorContexts.Commit)
+        updateTitle()
+    End Sub
+
+    Private Sub refreshUI()
+        Call LoadFromIDSet(From mol In list Select mol.id)
+    End Sub
+
+    Private Sub OpenInEditorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenInEditorToolStripMenuItem.Click
+        If DataGridView1.SelectedRows.Count = 0 Then
+            Return
+        End If
+
+        Dim sel As MoleculeBatchView = DataGridView1.SelectedRows(0).Tag
+
+        If sel Is Nothing Then
+            Return
+        End If
+
+        Call Workbench.OpenMoleculeEditor(sel.id, sel.name)
+    End Sub
+
+    Private Sub RemoveFromListToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RemoveFromListToolStripMenuItem.Click
+        If DataGridView1.SelectedRows.Count = 0 Then
+            Return
+        End If
+
+        Dim sel As MoleculeBatchView = DataGridView1.SelectedRows(0).Tag
+
+        If sel Is Nothing Then
+            Return
+        End If
+
+        list = list.Where(Function(a) a.id <> sel.id).ToArray
+        DataGridView1.Rows.Remove(DataGridView1.SelectedRows(0))
+        DataGridView1.CommitEdit(DataGridViewDataErrorContexts.Commit)
+        updateTitle()
+    End Sub
+
+    Private Sub updateTitle()
+        Text = $"Batch Editor [{list.Length} molecules]"
+    End Sub
+
+    Private Sub FormBatchEditor_Load(sender As Object, e As EventArgs) Handles Me.Load
+        For Each term As TopicTerm In TopicTerm.GetTopics
+            Call ComboBox1.Items.Add(term)
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' add
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ComboBox1.SelectedIndex < 0 Then
+            Return
+        End If
+
+        Dim term As TopicTerm = ComboBox1.SelectedItem
+
+        For Each mol As MoleculeBatchView In list
+            If MyApplication.biocad_registry.molecule_tags.where(field("tag_id") = term.id, field("molecule_id") = mol.id).find(Of biocad_registryModel.molecule_tags) Is Nothing Then
+                Call MyApplication.biocad_registry.molecule_tags.add(field("tag_id") = term.id, field("molecule_id") = mol.id, field("description") = "Batch Editor")
+            End If
+        Next
+
+        Call refreshUI()
+    End Sub
+
+    ''' <summary>
+    ''' remove
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If ComboBox1.SelectedIndex < 0 Then
+            Return
+        End If
+
+        Dim term As TopicTerm = ComboBox1.SelectedItem
+
+        For Each mol As MoleculeBatchView In list
+            MyApplication.biocad_registry.molecule_tags.where(field("tag_id") = term.id, field("molecule_id") = mol.id).delete()
+        Next
+
+        Call refreshUI()
     End Sub
 End Class
