27b93ff71077cd5513d7d811d4a6ae887b37a82e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-12 10:04:34 +0800

    filter data

--- src/biocad_storage/imports/BatchDataCommit/LotusNPImports.vb
+++ src/biocad_storage/imports/BatchDataCommit/LotusNPImports.vb
@@ -68,7 +68,7 @@ Public Class CoconutNPImports
     End Sub
 
     Public Sub ImportsNP(np As IEnumerable(Of CoconutNPTable))
-        For Each page As CoconutNPTable() In np.SplitIterator(25)
+        For Each page As CoconutNPTable() In np.SplitIterator(2500)
             Call importsPage(page)
         Next
     End Sub
