b1fa4d3ce1ee2e2f72bd317f6455bd4796e56849
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-02 16:50:49 +0800

    make bugs fixed the seql query for export enzyme protein sequence

--- src/biocad_storage/exports/ProtFasta.vb
+++ src/biocad_storage/exports/ProtFasta.vb
@@ -14,20 +14,24 @@ Public Module ProtFasta
     Public Iterator Function ExportEnzyme(registry As biocad_registry) As IEnumerable(Of FastaSeq)
         Dim page_size As Integer = 100
         Dim ec_id As UInteger = registry.vocabulary_terms.ecnumber_term
+        Dim last_id As UInteger = 0
+        Dim prot_key As UInteger = registry.vocabulary_terms.protein_term
 
         For page As Integer = 1 To Integer.MaxValue
-            Dim seqs = registry.db_xrefs _
+            Dim seqs_page = registry.db_xrefs _
                 .left_join("sequence_graph") _
                 .on(field("sequence_graph.molecule_id") = field("obj_id")) _
-                .where(field("db_key") = ec_id) _
-                .limit((page - 1) * page_size, page_size) _
+                .where(field("db_key") = ec_id, field("type") = prot_key, field("obj_id") > last_id) _
+                .limit(page_size) _
                 .select(Of EnzymeSequence)("obj_id as cad_id", "xref as ec_number", "sequence")
 
-            If seqs.IsNullOrEmpty Then
+            If seqs_page.IsNullOrEmpty Then
                 Exit For
+            Else
+                last_id = seqs_page.Select(Function(s) s.cad_id).Max
             End If
 
-            For Each seq As EnzymeSequence In seqs
+            For Each seq As EnzymeSequence In seqs_page
                 Yield New FastaSeq With {
                     .Headers = {
                         seq.ec_number,
