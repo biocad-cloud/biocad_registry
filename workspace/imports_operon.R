19fd14d729a0cc5ba516b3fb3024cfbc7c698a03
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-16 23:21:37 +0800

    Update imports_operon.R

--- workspace/imports_operon.R
+++ workspace/imports_operon.R
@@ -4,5 +4,6 @@ imports "data_imports" from "biocad_registry";
 imports "annotation.genomics" from "seqtoolkit";
 
 let operon = operon_set();
+let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 
-str(operon[1]);
+biocad_registry |> imports_operon(operon);
