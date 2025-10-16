6841ed260e9af116b766873751196a6cc01c782c
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-16 22:51:25 +0800

    commit operon data

---
+++ src/biocad_storage/imports/BatchDataCommit/CommitOperonCluster.vb
@@ -0,0 +1,64 @@
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar.Tqdm
+Imports Microsoft.VisualBasic.Linq
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+Imports SMRUCC.genomics.Model.OperonMapper
+
+Public Module CommitOperonCluster
+
+    Public Sub CommitOperonSet(registry As biocad_registry, operons As IEnumerable(Of OperonRow))
+        For Each page As OperonRow() In operons.Split(100)
+            Call CommitOperonPage(registry, page)
+        Next
+    End Sub
+
+    Private Sub CommitOperonPage(registry As biocad_registry, page As OperonRow())
+        Dim clusters = registry.conserved_cluster
+        Dim link = registry.cluster_link.open_transaction.ignore
+
+        For Each tu As OperonRow In TqdmWrapper.Wrap(page)
+            Dim check = clusters _
+                .where(field("db_xref") = tu.koid,
+                       field("tax_id") = tu.org) _
+                .find(Of biocad_registryModel.conserved_cluster)
+
+            If check Is Nothing Then
+                clusters.add(
+                    field("db_xref") = tu.koid,
+                    field("tax_id") = tu.org,
+                    field("name") = tu.name,
+                    field("size") = tu.op.TryCount,
+                    field("description") = tu.definition
+                )
+                check = clusters _
+                    .where(field("db_xref") = tu.koid,
+                           field("tax_id") = tu.org) _
+                    .find(Of biocad_registryModel.conserved_cluster)
+            End If
+            If check Is Nothing Then
+                Continue For
+            End If
+
+            For Each gene_id As String In tu.op.SafeQuery
+                Dim gene = registry.molecule _
+                    .where(field("tax_id") = tu.org,
+                           field("xref_id") = $"{tu.org}:{gene_id}") _
+                    .find(Of biocad_registryModel.molecule)
+
+                If gene Is Nothing Then
+                    Continue For
+                End If
+
+                If registry.cluster_link _
+                    .where(field("cluster_id") = check.id, field("gene_id") = gene.id) _
+                    .find(Of biocad_registryModel.cluster_link) Is Nothing Then
+
+                    Call link.add(field("cluster_id") = check.id,
+                                  field("gene_id") = gene.id)
+                End If
+            Next
+        Next
+
+        Call link.commit()
+    End Sub
+
+End Module
