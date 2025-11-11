d618ac483648fc3da527b50ed6046af777163a6e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-11 17:48:46 +0800

    genome sequence data query success

--- workspace/query_regprecise.R
+++ workspace/query_regprecise.R
@@ -2,4 +2,26 @@ require(GCModeller);
 
 imports "NCBI" from "annotationKit";
 
+let repo = list.files("F:\ecoli\regprecise", pattern = "*.xml",recursive = FALSE);
+let genome_names = basename(repo);
+
+print(genome_names);
+
 let db = genbank_assemblyDb("D:\datapool\assembly_summary_genbank.txt");
+let q = [];
+let index = [];
+
+for(let name in tqdm(genome_names)) {
+    let result = db |> NCBI::query(name, cutoff = 0.9);
+
+    index = append(index, result |> attr("index"));
+    q = append(q, result);    
+}
+
+q = unlist(q);
+index = unlist(index);
+
+print(as.data.frame(index));
+
+write.csv(q, file = relative_work("genomes.csv"));
+write.csv(index, file = relative_work("query_index.csv"));
