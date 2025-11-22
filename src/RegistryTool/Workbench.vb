cd84d0a595f89e6c3c48a1bf9295018baaafa92e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-22 16:13:27 +0800

    check of the input

--- src/RegistryTool/Workbench.vb
+++ src/RegistryTool/Workbench.vb
@@ -12,8 +12,15 @@ Module Workbench
     Public Sub OpenMoleculeEditor(id As String, name As String)
         Dim edit As New FormMoleculeEditor With {.id = id}
 
-        edit.MdiParent = MyApplication.host
-        edit.Text = $"BioCAD{id.PadLeft(11, "0"c)} - {name}"
-        edit.Show()
+        If id.IsPattern("\d+") OrElse id.IsPattern("BioCAD\d+") Then
+            edit.MdiParent = MyApplication.host
+            edit.Text = $"BioCAD{id.PadLeft(11, "0"c)} - {name}"
+            edit.Show()
+        Else
+            Call MessageBox.Show($"the requested id '{id}' is not a valid registry number!",
+                                 "Invalid input",
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Warning)
+        End If
     End Sub
 End Module
