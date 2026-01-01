ed9ba999a819c42f795641248daa0f6d830ab220
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-01 14:52:25 +0800

    Update build.cmd

--- mysql/build.cmd
+++ mysql/build.cmd
@@ -2,7 +2,8 @@ REM @echo off
 
 set reflector="\graphQL\src\mysqli\App\Reflector.exe"
 set R_src="../src/biocad_storage/mysql/mysql"
+set sql="./cad_registry-v2-20260101.sql"
 
-%reflector% --reflects /sql ./cad_registry20251107.sql -o %R_src% /namespace biocad_registryModel --language visualbasic /split /auto_increment.disable
+CALL %reflector% --reflects /sql %sql% -o %R_src% /namespace biocad_registryModel --language visualbasic /split /auto_increment.disable
 
 REM pause
