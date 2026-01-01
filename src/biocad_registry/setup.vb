b84d491d7cf788c47f22a45e9eee69c18d5136a1
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-01 17:18:04 +0800

    how to make kegg datbase setpup

--- src/biocad_registry/setup.vb
+++ src/biocad_registry/setup.vb
@@ -1,5 +1,11 @@
 ﻿
+Imports BioNovoGene.BioDeep.Chemistry.MetaLib
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
+Imports SMRUCC.Rsharp.Runtime.Interop
 
 ''' <summary>
 ''' The Initial setup of the database
@@ -8,6 +14,25 @@ Imports Microsoft.VisualBasic.Scripting.MetaData
 <Package("setup")>
 Public Module setup
 
+    ''' <summary>
+    ''' use kegg+refmet as template for make setup of the metabolites table
+    ''' </summary>
+    ''' <param name="kegg"></param>
+    ''' <param name="refmet"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
+    <ExportAPI("setup_metabolites")>
+    Public Function setup_metabolites(<RRawVectorArgument> kegg As Object, <RRawVectorArgument> refmet As Object, Optional env As Environment = Nothing) As Object
+        Dim keggLib As pipeline = pipeline.TryCreatePipeline(Of Compound)(kegg, env)
+        Dim refmetLib As pipeline = pipeline.TryCreatePipeline(Of RefMet)(refmet, env)
 
+        If keggLib.isError Then
+            Return keggLib.getError
+        ElseIf refmetLib.isError Then
+            Return refmetLib.getError
+        End If
+
+
+    End Function
 
 End Module
