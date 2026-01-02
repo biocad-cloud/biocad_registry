187c9e70b051a1190d0b4de35ca595e61fa2a0fa
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-02 11:32:40 +0800

    Update setup.R

--- test/2026_v2/setup.R
+++ test/2026_v2/setup.R
@@ -7,5 +7,5 @@ imports "setup" from "biocad_registry";
 
 let registry = open_registry("root","123456");
 
-# registry |> setup_kegg(GCModeller::kegg_compounds(rawList = TRUE,reference_set = FALSE));
+registry |> setup_kegg(GCModeller::kegg_compounds(rawList = TRUE,reference_set = FALSE));
 registry |> setup_refmet(read.RefMet("G:\biocad_registry\data\refmet.csv"));
