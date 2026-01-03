84c19c497cbd41f764cdae42d8a1f2cdd4d58bfa
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-03 14:11:05 +0800

    fix rule for create symbol name

--- src/registry_data/views/RegisterSymbol.vb
+++ src/registry_data/views/RegisterSymbol.vb
@@ -4,6 +4,6 @@ Public Module RegisterSymbol
 
     <Extension>
     Public Function makeSymbol(name As String) As String
-        Return Strings.Trim(name).Replace("(", "_").Replace(")", "_").Replace("""", "_").Replace("'", "_").Replace("\", "_").Replace("/", "_").Replace(" ", "_").StringReplace("_{2,}", "_")
+        Return Strings.Trim(name).Replace("(", "_").Replace(")", "_").Replace("""", "_").Replace("'", "_").Replace("\", "_").Replace("/", "_").StringReplace("\s", "_").StringReplace("[_-]{2,}", "_").Trim("-"c, "_"c)
     End Function
 End Module
