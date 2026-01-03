307d71021a4d0395c6907cbe6a2003a96ae5ec0d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-03 23:09:56 +0800

    make code style improvements

--- src/registry_data/ImportsMetabolite.vb
+++ src/registry_data/ImportsMetabolite.vb
@@ -5,7 +5,11 @@ Imports registry_data.biocad_registryModel
 Public Module ImportsMetabolite
 
     <Extension>
-    Public Sub SaveMetaboliteClass(registry As biocad_registry, m As metabolites, ontology_id As UInteger, ont As (kingdom$, super_class$, class$, sub_class$), source_id As String)
+    Public Sub SaveMetaboliteClass(registry As biocad_registry,
+                                   m As metabolites,
+                                   ontology_id As UInteger,
+                                   ont As (kingdom$, super_class$, class$, sub_class$),
+                                   source_id As String)
         Dim kingdom As ontology
         Dim super_class As ontology
         Dim [class] As ontology
@@ -13,44 +17,79 @@ Public Module ImportsMetabolite
         Dim node As ontology = Nothing
 
         If Not ont.kingdom.StringEmpty Then
-            kingdom = registry.ontology.where(field("ontology_id") = ontology_id, field("term_id") = ont.kingdom).find(Of ontology)
+            kingdom = registry.ontology _
+                .where(field("ontology_id") = ontology_id,
+                       field("term_id") = ont.kingdom) _
+                .find(Of ontology)
 
             If kingdom Is Nothing Then
-                registry.ontology.add(field("ontology_id") = ontology_id, field("term_id") = ont.kingdom, field("term") = ont.kingdom)
-                kingdom = registry.ontology.where(field("ontology_id") = ontology_id, field("term_id") = ont.kingdom).find(Of ontology)
+                registry.ontology.add(field("ontology_id") = ontology_id,
+                                      field("term_id") = ont.kingdom,
+                                      field("term") = ont.kingdom)
+                kingdom = registry.ontology _
+                    .where(field("ontology_id") = ontology_id,
+                           field("term_id") = ont.kingdom) _
+                    .find(Of ontology)
             End If
 
             If kingdom IsNot Nothing Then node = kingdom
 
             If kingdom IsNot Nothing AndAlso Not ont.super_class.StringEmpty Then
-                super_class = registry.ontology.where(field("ontology_id") = ontology_id, field("term_id") = ont.super_class).find(Of ontology)
+                super_class = registry.ontology _
+                    .where(field("ontology_id") = ontology_id,
+                           field("term_id") = ont.super_class) _
+                    .find(Of ontology)
 
                 If super_class Is Nothing Then
-                    registry.ontology.add(field("ontology_id") = ontology_id, field("term_id") = ont.super_class, field("term") = ont.super_class)
-                    super_class = registry.ontology.where(field("ontology_id") = ontology_id, field("term_id") = ont.super_class).find(Of ontology)
-                    registry.ontology_relation.add(field("term_id") = super_class.id, field("is_a") = kingdom.id)
+                    registry.ontology.add(field("ontology_id") = ontology_id,
+                                          field("term_id") = ont.super_class,
+                                          field("term") = ont.super_class)
+                    super_class = registry.ontology _
+                        .where(field("ontology_id") = ontology_id,
+                               field("term_id") = ont.super_class) _
+                        .find(Of ontology)
+                    registry.ontology_relation.add(field("term_id") = super_class.id,
+                                                   field("is_a") = kingdom.id)
                 End If
 
                 If super_class IsNot Nothing Then node = super_class
 
                 If super_class IsNot Nothing AndAlso Not ont.class.StringEmpty Then
-                    [class] = registry.ontology.where(field("ontology_id") = ontology_id, field("term_id") = ont.class).find(Of ontology)
+                    [class] = registry.ontology _
+                        .where(field("ontology_id") = ontology_id,
+                               field("term_id") = ont.class) _
+                        .find(Of ontology)
 
                     If [class] Is Nothing Then
-                        registry.ontology.add(field("ontology_id") = ontology_id, field("term_id") = ont.class, field("term") = ont.class)
-                        [class] = registry.ontology.where(field("ontology_id") = ontology_id, field("term_id") = ont.class).find(Of ontology)
-                        registry.ontology_relation.add(field("term_id") = [class].id, field("is_a") = super_class.id)
+                        registry.ontology.add(field("ontology_id") = ontology_id,
+                                              field("term_id") = ont.class,
+                                              field("term") = ont.class)
+                        [class] = registry.ontology _
+                            .where(field("ontology_id") = ontology_id,
+                                   field("term_id") = ont.class) _
+                            .find(Of ontology)
+                        registry.ontology_relation.add(field("term_id") = [class].id,
+                                                       field("is_a") = super_class.id)
                     End If
 
                     If [class] IsNot Nothing Then node = [class]
 
                     If [class] IsNot Nothing AndAlso Not ont.sub_class.StringEmpty Then
-                        sub_class = registry.ontology.where(field("ontology_id") = ontology_id, field("term_id") = ont.sub_class).find(Of ontology)
+                        sub_class = registry.ontology _
+                            .where(field("ontology_id") = ontology_id,
+                                   field("term_id") = ont.sub_class) _
+                            .find(Of ontology)
 
                         If sub_class Is Nothing Then
-                            registry.ontology.add(field("ontology_id") = ontology_id, field("term_id") = ont.sub_class, field("term") = ont.sub_class)
-                            sub_class = registry.ontology.where(field("ontology_id") = ontology_id, field("term_id") = ont.sub_class).find(Of ontology)
-                            registry.ontology_relation.add(field("term_id") = sub_class.id, field("is_a") = [class].id)
+                            registry.ontology.add(field("ontology_id") = ontology_id,
+                                                  field("term_id") = ont.sub_class,
+                                                  field("term") = ont.sub_class)
+                            sub_class = registry.ontology _
+                                .where(field("ontology_id") = ontology_id,
+                                       field("term_id") = ont.sub_class) _
+                                .find(Of ontology)
+                            registry.ontology_relation.add(field("term_id") = sub_class.id,
+                                                           field("is_a") = [class].id)
                         End If
 
                         node = sub_class
