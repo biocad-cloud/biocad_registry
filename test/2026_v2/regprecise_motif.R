def31e1d55608593d1aa8e0a3268ac20e1792ad2
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-05 13:25:52 +0800

    config product server

--- test/2026_v2/regprecise_motif.R
+++ test/2026_v2/regprecise_motif.R
@@ -4,7 +4,7 @@ require(GCModeller);
 imports "setup" from "biocad_registry";
 imports "regprecise" from "TRNtoolkit";
 
-let registry = open_registry("root","123456");
+let registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 let motif_db = read.regprecise("F:\ecoli\regprecise.xml");
 
 registry |> save_motif(motif_db);
