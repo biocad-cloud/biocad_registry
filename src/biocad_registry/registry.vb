0653a5f54a0980465cd35c739d387193424e928e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-06 00:40:49 +0800

    save ncbi genbank

--- src/biocad_registry/registry.vb
+++ src/biocad_registry/registry.vb
@@ -1,7 +1,31 @@
 ﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports registry_data
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
+Imports SMRUCC.Rsharp.Runtime.Interop
 
 <Package("registry")>
 Module registry
 
+    <ExportAPI("save_genbank")>
+    Public Function save_genbank(registry As biocad_registry,
+                                 <RRawVectorArgument>
+                                 genbank As Object,
+                                 Optional env As Environment = Nothing) As Object
+
+        Dim pull As pipeline = pipeline.TryCreatePipeline(Of GBFF.File)(genbank, env)
+
+        If pull.isError Then
+            Return pull.getError
+        End If
+
+        For Each gb_asm As GBFF.File In genbank
+            Call registry.SaveGenBank(gb_asm)
+        Next
+
+        Return Nothing
+    End Function
 End Module
