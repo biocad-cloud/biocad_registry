caaaa30304e8f0040eaa9acbe704752c2ff2f7db
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-08 15:08:12 +0800

    export smiles data

---
+++ src/registry_data/exports/MetaboliteData.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+
+Namespace Exports
+
+    Public Module MetaboliteData
+
+        <Extension>
+        Public Iterator Function exportSMILES(registry As biocad_registry, dbname As String) As IEnumerable(Of SMILESData)
+            Dim page_size As Integer = 2000
+            Dim offset As UInteger
+            Dim page_data As SMILESData()
+
+            For i As Integer = 1 To Integer.MaxValue
+                offset = (i - 1) * page_size
+                page_data = registry.metabolites _
+                    .left_join("struct_data") _
+                    .on(field("`struct_data`.metabolite_id") = field("`metabolites`.id")) _
+                    .where(Not field(dbname).is_nothing, field(dbname) <> "") _
+                    .limit(offset, page_size) _
+                    .select(Of SMILESData)("hmdb_id AS xref_id",
+                                           "smiles",
+                                           "CONCAT('BioCAD', LPAD(metabolite_id, 11, '0')) AS id",
+                                           "name",
+                                           "formula",
+                                           "exact_mass")
+                If page_data.IsNullOrEmpty Then
+                    Exit For
+                Else
+                    For Each data As SMILESData In page_data
+                        Yield data
+                    Next
+                End If
+            Next
+        End Function
+    End Module
+End Namespace
