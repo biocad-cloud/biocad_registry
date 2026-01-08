c10ed4dd27283c0ac16ab9637e29118f2086d6fb
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-08 23:38:00 +0800

    make exports of the simple metabolites tbale

---
+++ src/biocad_registry/exports.vb
@@ -0,0 +1,46 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+Imports registry_data
+Imports registry_data.biocad_registryModel
+Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
+
+<Package("exports")>
+Module exports
+
+    <ExportAPI("metabolite_table")>
+    Public Function export_metabolite_table(registry As biocad_registry, Optional dbname As String = Nothing) As Object
+        Dim table As metabolites()
+
+        If dbname.StringEmpty Then
+            table = registry.metabolites.where(field("exact_mass") > 0).select(Of metabolites)
+        Else
+            table = registry.metabolites.where(Not field(dbname).is_nothing, field(dbname) <> "").select(Of metabolites)
+        End If
+
+        Dim df As New dataframe With {
+            .rownames = table _
+                .Select(Function(a) "BioCAD" & a.id.ToString.PadLeft(11, "0"c)) _
+                .ToArray,
+            .columns = New Dictionary(Of String, Array)
+        }
+
+        Call df.add("id", df.rownames)
+        Call df.add("name", From m As metabolites In table Select m.name)
+        Call df.add("formula", From m As metabolites In table Select m.formula)
+        Call df.add("exact_mass", From m As metabolites In table Select m.exact_mass)
+        Call df.add("cas_id", From m As metabolites In table Select m.cas_id)
+        Call df.add("pubchem_id", From m As metabolites In table Select m.pubchem_cid)
+        Call df.add("chebi_id", From m As metabolites In table Select "ChEBI:" & m.chebi_id)
+        Call df.add("hmdb_id", From m As metabolites In table Select m.hmdb_id)
+        Call df.add("lipidmaps_id", From m As metabolites In table Select m.lipidmaps_id)
+        Call df.add("kegg_id", From m As metabolites In table Select m.kegg_id)
+        Call df.add("drugbank_id", From m As metabolites In table Select m.drugbank_id)
+        Call df.add("biocyc", From m As metabolites In table Select m.biocyc)
+        Call df.add("mesh_id", From m As metabolites In table Select m.mesh_id)
+        Call df.add("wikipedia", From m As metabolites In table Select m.wikipedia)
+
+        Return df
+    End Function
+
+End Module
