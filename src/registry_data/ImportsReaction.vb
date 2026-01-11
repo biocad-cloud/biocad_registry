146a1aa660e647b4d8ea83e4560d70f283e625df
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-11 15:18:08 +0800

    needs imports metacyc

---
+++ src/registry_data/ImportsReaction.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.ComponentModel.EquaionModel
+
+Public Module ImportsReaction
+
+    <Extension>
+    Public Sub importsReactions(registry As biocad_registry, reactions As IEnumerable(Of Reaction))
+
+    End Sub
+End Module
