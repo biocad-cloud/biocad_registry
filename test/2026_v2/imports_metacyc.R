d147579a1da147a6a4091adb459f32e2a2de5316
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-12 16:51:24 +0800

    run imports test success

--- test/2026_v2/imports_metacyc.R
+++ test/2026_v2/imports_metacyc.R
@@ -4,6 +4,6 @@ require(GCModeller);
 imports "registry" from "biocad_registry";
 
 let registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
-let metacyc = open.biocyc("F:\ecoli\29.0\data");
+let metacyc = open.biocyc("F:\ecoli\gcf_002734145\29.0");
 
 registry |> imports_metacyc_compounds(metacyc);
