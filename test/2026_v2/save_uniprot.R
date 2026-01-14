73be22628fdf825dce5fab4865ef51dc3760f89f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-14 10:43:15 +0800

    config run imports of ecoli uniprot dataset

--- test/2026_v2/save_uniprot.R
+++ test/2026_v2/save_uniprot.R
@@ -2,7 +2,7 @@ require(biocad_registry);
 
 imports "registry" from "biocad_registry";
 
-let uniprot = open.uniprot("J:\uniprot-all.xml\92938b97fc8aeaef3291c581f36554cc0301be29.xml");
+let uniprot = open.uniprot("G:\biocad_registry\test\uniprotkb_taxonomy_id_2_AND_model_organ_2024_08_02.xml");
 let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 
 biocad_registry |> save_uniprot(uniprot);
