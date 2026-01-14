570fa674089eb6e5791da2c81592b633a5863619
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-14 21:32:16 +0800

    run package msbuild

--- test/2026_v2/save_uniprot.R
+++ test/2026_v2/save_uniprot.R
@@ -7,8 +7,9 @@ imports "registry" from "biocad_registry";
 
 # let uniprot = open.uniprot("U:\uniprot_sprot_archaea.xml");
 # let uniprot = open.uniprot("U:\uniprot_sprot_bacteria.xml");
-let uniprot = open.uniprot("U:\uniprot_sprot_fungi.xml");
+# let uniprot = open.uniprot("U:\uniprot_sprot_fungi.xml");
 
+let uniprot = open.uniprot("U:\uniprot_trembl_bacteria.xml");
 let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 
 biocad_registry |> save_uniprot(uniprot);
