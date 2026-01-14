376f8bce894229debb7649a45b77287740309fdf
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-14 12:17:53 +0800

    Update save_uniprot.R

--- test/2026_v2/save_uniprot.R
+++ test/2026_v2/save_uniprot.R
@@ -2,7 +2,9 @@ require(biocad_registry);
 
 imports "registry" from "biocad_registry";
 
-let uniprot = open.uniprot("U:\fungi-20251120\Saccharomycetes.xml");
+# let uniprot = open.uniprot("U:\fungi-20251120\Saccharomycetes.xml");
+
+let uniprot = open.uniprot("U:\fungi-20251120\Eurotiomycetes.xml");
 let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 
 biocad_registry |> save_uniprot(uniprot);
