c135e8ffd72954abca57645aa9c845ce84f9d743
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-16 13:04:50 +0800

    create models for export data

---
+++ src/registry_exports/ExportVirtualCellModels.vb
@@ -0,0 +1,18 @@
+﻿Imports Oracle.LinuxCompatibility.MySQL.Reflection.DbAttributes
+Imports registry_data
+
+Public Class ExportVirtualCellModels
+
+    ReadOnly registry As biocad_registry
+    ReadOnly repo As String
+    ReadOnly vocabulary As biocad_vocabulary
+
+    Sub New(registry As biocad_registry, repo As String)
+        Me.vocabulary = registry.biocad_vocabulary
+        Me.repo = repo
+        Me.registry = registry
+    End Sub
+
+
+
+End Class
---
+++ src/registry_exports/WebJSON/localcacheViews.vb
@@ -0,0 +1,34 @@
+﻿Imports Oracle.LinuxCompatibility.MySQL.Reflection.DbAttributes
+
+Namespace localcacheViews
+
+    Public Class xref_query
+        <DatabaseField> Public Property dbname As String
+        <DatabaseField> Public Property xref_id As String
+    End Class
+
+    Public Class kinetics_args
+
+        <DatabaseField> Public Property params As String
+        <DatabaseField> Public Property lambda As String
+        <DatabaseField> Public Property metabolite_id As UInteger
+        <DatabaseField> Public Property json_str As String
+
+    End Class
+
+    Public Class reaction_group
+
+        <DatabaseField> Public Property hashcode As String
+        <DatabaseField> Public Property reactions As String
+
+    End Class
+
+    Public Class operonData
+
+        <DatabaseField> Public Property cluster_id As UInteger
+        <DatabaseField> Public Property name As String
+        <DatabaseField> Public Property members As String
+
+    End Class
+
+End Namespace
