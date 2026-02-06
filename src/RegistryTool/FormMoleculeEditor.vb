db6dd08783c040bddfe613e51ca2132ecfd99d93
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-06 16:59:49 +0800

    create ui for edit main xrefs id

--- src/RegistryTool/FormMoleculeEditor.vb
+++ src/RegistryTool/FormMoleculeEditor.vb
@@ -706,6 +706,10 @@ let options = { width: 450, height: 300 };
            )
         Next
     End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+
+    End Sub
 End Class
 
 Public Class reaction_graphdata
