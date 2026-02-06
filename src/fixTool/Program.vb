2534d2c74308ab868d9abae1d125e950de9a9e79
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-06 17:43:02 +0800

    clean mona name

--- src/fixTool/Program.vb
+++ src/fixTool/Program.vb
@@ -2,6 +2,7 @@ Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar.Tqdm
 Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
 Imports Oracle.LinuxCompatibility.MySQL.Uri
 Imports registry_data
+Imports SMRUCC.genomics.Data.Regtransbase.WebServices.Regulogs
 
 Module Program
 
@@ -17,6 +18,36 @@ Module Program
 
     Sub Main(args As String())
         Call removesInvalidNameChars()
+        Call fixMoNANames()
+    End Sub
+
+    Sub fixMoNANames()
+        Dim sel = "SELECT 
+    *
+FROM
+    metabolites
+WHERE
+    id IN (SELECT DISTINCT
+            obj_id
+        FROM
+            cad_registry.db_xrefs
+        WHERE
+            db_name = 742)
+        AND INSTR(name, 'NCGC') = 1
+ORDER BY id DESC"
+
+        Dim q = registry.metabolites.getDriver.Query(Of biocad_registryModel.metabolites)(sel)
+        Dim update_names = registry.metabolites.ignore.open_transaction
+
+        For Each invalid In TqdmWrapper.Wrap(q)
+            Dim clean_name = invalid.name.StringReplace("NCGC\d+[-]\d+", "").Trim(" "c, "!"c, """"c, "-"c, "_"c)
+            Call update_names.add(registry.metabolites _
+                   .where(field("id") = invalid.id) _
+                   .save_sql(field("name") = clean_name,
+                             field("hashcode") = clean_name.ToLower.MD5))
+        Next
+
+        Call update_names.commit()
     End Sub
 
     Sub removesInvalidNameChars()
