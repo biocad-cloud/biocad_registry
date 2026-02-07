9ddfef39cbb8c77c1003a44b18714196879251e2
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-07 23:28:18 +0800

    settings config folder

---
+++ src/RegistryTool/configs/MoleculeEditHistory.vb
@@ -0,0 +1,13 @@
+﻿Namespace Configs
+
+    Public Class MoleculeEditHistory
+
+        Public Property id As String
+        Public Property name As String
+
+        Public Overrides Function ToString() As String
+            Return $"{id} - {name}"
+        End Function
+
+    End Class
+End Namespace
--- src/RegistryTool/configs/Settings.vb
+++ src/RegistryTool/configs/Settings.vb
@@ -51,15 +51,4 @@ Namespace Configs
         End Function
 
     End Class
-
-    Public Class MoleculeEditHistory
-
-        Public Property id As String
-        Public Property name As String
-
-        Public Overrides Function ToString() As String
-            Return $"{id} - {name}"
-        End Function
-
-    End Class
 End Namespace
