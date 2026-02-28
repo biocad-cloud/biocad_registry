4838fbf78724d999f1f110b02b4801d18424cb31
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-28 19:21:49 +0800

    make code style improvements

--- src/registry_data/LLMs/TranslatedName.vb
+++ src/registry_data/LLMs/TranslatedName.vb
@@ -1,5 +1,4 @@
 ﻿Imports Microsoft.VisualBasic.Serialization.JSON
-Imports Ollama
 
 Public Class TranslatedName
 
@@ -12,19 +11,23 @@ Public Class TranslatedName
     Public Shared Function DecodeLLMTranslateOutput(llms_output As String) As String
         If Not llms_output Is Nothing Then
             Try
-                Dim json As String = llms_output.Match("[{].+[}]")
-                Dim zh_name = json.LoadJSON(Of TranslatedName)
-
-                If zh_name Is Nothing Then
-                    Return Nothing
-                Else
-                    Return zh_name.zh_name
-                End If
+                Return DecodeJSON(llms_output)
             Catch ex As Exception
                 Return Nothing
             End Try
         Else
             Return Nothing
+        End If
+    End Function
+
+    Private Shared Function DecodeJSON(llms_output As String) As String
+        Dim json As String = llms_output.Match("[{].+[}]")
+        Dim zh_name = json.LoadJSON(Of TranslatedName)
+
+        If zh_name Is Nothing Then
+            Return Nothing
+        Else
+            Return zh_name.zh_name
         End If
     End Function
 
