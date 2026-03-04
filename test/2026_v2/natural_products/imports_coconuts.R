a4eebc12b342925cd53e0e0d6e4c8ae2e7d3ad6c
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-05 01:36:28 +0800

    Update imports_coconuts.R

--- test/2026_v2/natural_products/imports_coconuts.R
+++ test/2026_v2/natural_products/imports_coconuts.R
@@ -6,3 +6,6 @@ imports "massbank" from "mzkit";
 imports "registry" from "biocad_registry";
 
 let registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
+let coconut = read.coconut("D:\datapool\natural_products\coconut\coconut_csv-08-2025.csv");
+
+registry |> imports_coconut(coconut);
