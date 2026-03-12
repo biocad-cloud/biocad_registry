525c7d08d1dca12744c31571cb14a64b8ea3a107
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-12 16:52:14 +0800

    hand,ing of the possible null reference error

--- src/biocad_registry/models.vb
+++ src/biocad_registry/models.vb
@@ -116,8 +116,8 @@ Public Module registry_models
                     field("name") = pwy.name,
                     field("type") = pwy.pwtype,
                     field("taxid") = CInt(Val(pwy.taxid)),
-                    field("dois") = pwy.dois.GetJson,
-                    field("note") = pwy.citations.JoinBy("; ")
+                    field("dois") = pwy.dois.SafeQuery.Where(Function(did) Not did.StringEmpty(, True)).GetJson,
+                    field("note") = pwy.citations.Where(Function(ct) Not ct.StringEmpty(, True)).JoinBy("; ")
                 )
                 check = registry.pathway _
                     .where(field("db_source") = source_db,
