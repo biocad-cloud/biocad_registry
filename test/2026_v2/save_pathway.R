8540f64e9ab52beb7a127cf6a31e4754330c6fd4
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-12 23:38:23 +0800

    Update save_pathway.R

--- test/2026_v2/save_pathway.R
+++ test/2026_v2/save_pathway.R
@@ -5,6 +5,12 @@ imports "models" from "biocad_registry";
 imports "pubchem_kit" from "mzkit";
 
 let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
-let pathways = parse_pathway_graph(readText("F:\PubChem_pathway_biocyc.json"));
 
-biocad_registry |> imports_pathways(pathways );
+for(file in ["F:\pathways\PubChem_pathway_Plant_Reactome__Data_Source__Plant_Reactome.json"
+"F:\pathways\PubChem_pathway_PlantCyc__Data_Source__PlantCyc.json"
+"F:\pathways\PubChem_pathway_reactome.json"
+"F:\pathways\PubChem_pathway_wikipathway.json"
+"F:\pathways\PubChem_pathway_PathBank__Data_Source__PathBank.json"]) {
+
+    biocad_registry |> imports_pathways(parse_pathway_graph(readText(file)) );
+}
