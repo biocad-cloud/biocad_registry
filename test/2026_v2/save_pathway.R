a8d3c8c0de8886bc18ea6c86f756143a688f172d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-12 18:39:32 +0800

    Update save_pathway.R

--- test/2026_v2/save_pathway.R
+++ test/2026_v2/save_pathway.R
@@ -5,6 +5,6 @@ imports "models" from "biocad_registry";
 imports "pubchem_kit" from "mzkit";
 
 let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
-let pathways = parse_pathway_graph(readText("U:\pubchem\PubChem_pathway_text_Escherichia coli.json"));
+let pathways = parse_pathway_graph(readText("F:\PubChem_pathway_biocyc.json"));
 
 biocad_registry |> imports_pathways(pathways );
