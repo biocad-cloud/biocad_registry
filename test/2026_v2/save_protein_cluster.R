3a25db7155a762fc21a622c2ed8bbc7950ee0231
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-19 10:24:30 +0800

    save the protein cluster alignment result

---
+++ test/2026_v2/save_protein_cluster.R
@@ -0,0 +1,9 @@
+require(biocad_registry);
+
+imports "annotation.workflow" from "seqtoolkit";
+imports "models" from "biocad_registry";
+
+let alignments = read_m8(file = "K:\\protein_clusters.tsv", stream = TRUE); 
+let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
+
+biocad_registry |> imports_diamond(blastp = alignments);
