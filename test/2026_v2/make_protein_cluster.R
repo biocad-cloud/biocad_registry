3dbdad7e484a1a61045f51131f970183a728e59b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-21 00:44:33 +0800

    Update make_protein_cluster.R

--- test/2026_v2/make_protein_cluster.R
+++ test/2026_v2/make_protein_cluster.R
@@ -2,7 +2,12 @@ require(biocad_registry);
 
 imports "annotation.workflow" from "seqtoolkit";
 imports "models" from "biocad_registry";
+imports "hmmer" from "seqtoolkit";
 
-open_registry("xieguigang", 123456, host ="192.168.3.15")
-|> make_protein_clusters()
+# open_registry("xieguigang", 123456, host ="192.168.3.15")
+# |> make_protein_clusters()
+# ;
+
+open_registry("xieguigang", 123456, host ="192.168.3.15") 
+|> link_prot_ko(hmmer::parse_kofamscan("M:\\KEGG\\ko.tsv"))
 ;
