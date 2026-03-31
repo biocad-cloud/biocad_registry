dd3bbae13311cc994b49f4fe58867776abdecec8
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-31 21:26:31 +0800

    Update save_protein_cluster.R

--- test/2026_v2/save_protein_cluster.R
+++ test/2026_v2/save_protein_cluster.R
@@ -6,4 +6,4 @@ imports "models" from "biocad_registry";
 let alignments = read_m8(file = "K:\\protein_clusters.tsv", stream = TRUE); 
 let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 
-biocad_registry |> imports_diamond(blastp = alignments, batch_size = 1000000);
+biocad_registry |> imports_diamond(blastp = alignments, batch_size = 100000,  check_unique = TRUE);
