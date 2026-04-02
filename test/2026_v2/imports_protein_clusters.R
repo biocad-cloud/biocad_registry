9dcb40f3064bfb5e280fa19424a897d99071ef8e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-02 22:19:04 +0800

    run package msbuyild

---
+++ test/2026_v2/imports_protein_clusters.R
@@ -0,0 +1,7 @@
+require(biocad_registry);
+
+let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
+let rep = "K:\\diamonds";
+let mysql = "C:\\Program Files\\MySQL\\MySQL Workbench 8.0 CE\\mysql.exe";
+
+biocad_registry |> imports_sql(rep, mysql);
