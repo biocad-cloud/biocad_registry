61f7cb73d556f9994043bbd61565c09fc45e1f61
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-02 23:55:49 +0800

    run msbuild

--- test/2026_v2/save_protein_cluster.R
+++ test/2026_v2/save_protein_cluster.R
@@ -7,4 +7,4 @@ let alignments = read_m8(file = "K:\\protein_clusters.tsv", stream = TRUE);
 # let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 
 # biocad_registry |> imports_diamond(blastp = alignments, batch_size = 100000,  check_unique = TRUE);
-diamond_transaction(blastp = alignments, dir = "K:\diamonds" ,batch_size  = 50000000);
+diamond_transaction(blastp = alignments, dir = "K:\diamonds_small" ,batch_size  = 500000);
