163a24cba5231e573ee27538461642ce34be5279
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-02 15:12:51 +0800

    split source file intoi multiple parts

---
+++ src/biocad_registry/imports/DiamondDump.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Runtime.CompilerServices
+Imports registry_data.biocad_registryModel
+Imports SMRUCC.genomics.Interops.NCBI.Extensions
+
+Module DiamondDump
+
+    <Extension>
+    Public Iterator Function CreateDiamondDumpData(block As DiamondAnnotation()) As IEnumerable(Of protein_cluster)
+        For Each d As DiamondAnnotation In block
+            Dim qid As UInteger = d.QseqId.Split("|"c).Last
+            Dim sid As UInteger = d.SseqId.Split("|"c).Last
+
+            Yield New protein_cluster With {
+                .bit_score = d.BitScore,
+                .e_value = d.EValue,
+                .gap_open = d.GapOpen,
+                .identities = d.Pident,
+                .mis_match = d.Mismatch,
+                .q_end = d.QEnd,
+                .q_start = d.QStart,
+                .s_end = d.SEnd,
+                .s_start = d.SStart,
+                .hit_id = sid,
+                .query_id = qid,
+                .add_time = Now
+            }
+        Next
+    End Function
+End Module
--- src/biocad_registry/models.vb
+++ src/biocad_registry/models.vb
@@ -7,8 +7,8 @@ Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Parallel.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports Microsoft.VisualBasic.Serialization.JSON
-Imports Oracle.LinuxCompatibility.MySQL
 Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+Imports Oracle.LinuxCompatibility.MySQL.Workbench
 Imports registry_data
 Imports registry_data.biocad_registryModel
 Imports SMRUCC.genomics.Analysis.SequenceTools.HMMER
@@ -243,25 +243,9 @@ Public Module registry_models
         Dim part As i32 = 1
 
         For Each block As DiamondAnnotation() In pull.populates(Of DiamondAnnotation)(env).SplitIterator(batch_size)
-            Call block.Select(Function(d)
-                                  Dim qid As UInteger = d.QseqId.Split("|"c).Last
-                                  Dim sid As UInteger = d.SseqId.Split("|"c).Last
-
-                                  Return New protein_cluster With {
-                                     .bit_score = d.BitScore,
-                                     .e_value = d.EValue,
-                                     .gap_open = d.GapOpen,
-                                     .identities = d.Pident,
-                                     .mis_match = d.Mismatch,
-                                     .q_end = d.QEnd,
-                                     .q_start = d.QStart,
-                                     .s_end = d.SEnd,
-                                     .s_start = d.SStart,
-                                     .hit_id = sid,
-                                     .query_id = qid,
-                                     .add_time = Now
-                                  }
-                              End Function).DumpLargeTransaction(path:=$"{dir}/diamond_{++part}.sql", distinct:=False)
+            Call block _
+                .CreateDiamondDumpData _
+                .DumpLargeTransaction(path:=$"{dir}/diamond_{++part}.sql", distinct:=False)
         Next
 
         Return Nothing
