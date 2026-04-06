08c074ccda750ef86d7a6a2566df01901c28a2a1
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-06 21:37:01 +0800

    Update make_vcdb.R

--- scripts/make_vcdb.R
+++ scripts/make_vcdb.R
@@ -5,4 +5,13 @@ imports "exports" from "biocad_registry";
 let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 let repo = relative_work("./localdb");
 
-exports::virtualcell_componentdb(biocad_registry, repo);
+biocad_registry |> exports::virtualcell_componentdb(repo,
+
+    tfbs = FALSE,
+    tf = FALSE,
+    cc = FALSE,
+    ec = FALSE,
+    rxn = TRUE,
+    metab = TRUE
+
+);
