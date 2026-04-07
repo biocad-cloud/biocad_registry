ce4a5ca5fb27b6112752218ac25bb042bbcd0a31
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-07 14:34:52 +0800

    Update make_protein_cluster.R

--- test/2026_v2/make_protein_cluster.R
+++ test/2026_v2/make_protein_cluster.R
@@ -6,5 +6,5 @@ imports "hmmer" from "seqtoolkit";
 
 let registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
 
-registry |> make_protein_clusters(cutoff = 50);
+registry |> make_protein_clusters(eval_cutoff = 1e-10);
 # registry |> link_prot_ko(hmmer::parse_kofamscan("K:\ko_result.tsv"));
