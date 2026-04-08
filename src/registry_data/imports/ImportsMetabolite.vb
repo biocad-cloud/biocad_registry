9312b17e28ea862269b3ce3d7f491aedb5c60418
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-08 11:54:47 +0800

    make code style improvements

--- src/registry_data/imports/ImportsMetabolite.vb
+++ src/registry_data/imports/ImportsMetabolite.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar.Tqdm
+Imports Microsoft.Win32
 Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
 Imports registry_data.biocad_registryModel
 Imports std = System.Math
@@ -172,7 +173,7 @@ Public Module ImportsMetabolite
         End If
     End Function
 
-    Private Sub UpdateHashCode(registry As biocad_registry, Optional page_size As Integer = 10000)
+    Private Sub UpdateHashCode(registry As biocad_registry, Optional page_size As Integer = 500000)
         For page As Integer = 1 To Integer.MaxValue
             Dim offset As UInteger = (page - 1) * page_size
             Dim page_data = registry.metabolites.limit(offset, page_size).select(Of metabolites)()
@@ -191,9 +192,13 @@ Public Module ImportsMetabolite
                     Call $"processing empty name of metabolite {name.id} '{name.name}'".debug
                     update_name = True
                     name.name = name.PickName
-                ElseIf name.name.IsUpperName Then
-                    update_name = True
-                    name.name = name.name.ToLower
+                    ' invalid update logic for
+                    ' XXXX-HETE
+                    ' ATP
+                    ' XXXXX-UMP
+                    'ElseIf name.name.IsUpperName AndAlso name.name.Length > 9 Then
+                    '    update_name = True
+                    '    name.name = name.name.ToLower
                 End If
 
                 name.name = RegisterSymbol.CleanName(name.name)
@@ -223,6 +228,112 @@ Public Module ImportsMetabolite
     End Function
 
     <Extension>
+    Private Sub ResolveMetaboliteAlias(metas As metabolites(), registry As biocad_registry, trans As CommitTransaction)
+        Dim top_main As metabolites = metas.OrderBy(Function(a) a.id).First
+        Dim struct = registry.struct_data.where(field("metabolite_id") = top_main.id).find(Of struct_data)
+
+        For Each meta As metabolites In metas
+            If std.Abs(meta.exact_mass - top_main.exact_mass) > 0.1 Then
+                Continue For
+            End If
+
+            If meta.id = top_main.id Then
+                Call trans.add(registry.metabolites.where(field("id") = top_main.id).save_sql(field("main_id") = 0))
+            Else
+                Dim updates As New List(Of FieldAssert)(FieldUpdates(top_main:=top_main, meta:=meta))
+
+                Call trans.add(registry.metabolites _
+                    .where(field("id") = meta.id) _
+                    .save_sql(field("main_id") = top_main.id))
+
+                If updates.Any Then
+                    Call trans.add(registry.metabolites _
+                        .where(field("id") = top_main.id) _
+                        .save_sql(updates.ToArray))
+                End If
+
+                If struct Is Nothing Then
+                    Dim smiles_str = registry.struct_data.where(field("metabolite_id") = meta.id).find(Of struct_data)
+
+                    If Not smiles_str Is Nothing Then
+                        Call registry.struct_data.add(
+                            field("metabolite_id") = top_main.id,
+                            field("checksum") = smiles_str.checksum,
+                            field("smiles") = smiles_str.smiles
+                        )
+
+                        struct = smiles_str
+                    End If
+                End If
+            End If
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' Update empty main data from the alias meta data
+    ''' </summary>
+    ''' <param name="top_main"></param>
+    ''' <param name="meta"></param>
+    ''' <returns></returns>
+    Private Iterator Function FieldUpdates(top_main As metabolites, meta As metabolites) As IEnumerable(Of FieldAssert)
+        If top_main.cas_id.StringEmpty AndAlso Not meta.cas_id.StringEmpty Then
+            top_main.cas_id = meta.cas_id
+
+            Yield (field(NameOf(metabolites.cas_id)) = meta.cas_id)
+        End If
+        If top_main.kegg_id.StringEmpty AndAlso Not meta.kegg_id.StringEmpty Then
+            top_main.kegg_id = meta.kegg_id
+
+            Yield (field(NameOf(metabolites.kegg_id)) = meta.kegg_id)
+        End If
+        If top_main.hmdb_id.StringEmpty AndAlso Not meta.hmdb_id.StringEmpty Then
+            top_main.hmdb_id = meta.hmdb_id
+
+            Yield (field(NameOf(metabolites.hmdb_id)) = meta.hmdb_id)
+        End If
+        If top_main.lipidmaps_id.StringEmpty AndAlso Not meta.lipidmaps_id.StringEmpty Then
+            top_main.lipidmaps_id = meta.lipidmaps_id
+
+            Yield (field(NameOf(metabolites.lipidmaps_id)) = meta.lipidmaps_id)
+        End If
+        If top_main.drugbank_id.StringEmpty AndAlso Not meta.drugbank_id.StringEmpty Then
+            top_main.drugbank_id = meta.drugbank_id
+
+            Yield (field(NameOf(metabolites.drugbank_id)) = meta.drugbank_id)
+        End If
+        If top_main.mesh_id.StringEmpty AndAlso Not meta.mesh_id.StringEmpty Then
+            top_main.mesh_id = meta.mesh_id
+
+            Yield (field(NameOf(metabolites.mesh_id)) = meta.mesh_id)
+        End If
+        If top_main.biocyc.StringEmpty AndAlso Not meta.biocyc.StringEmpty Then
+            top_main.biocyc = meta.biocyc
+
+            Yield (field(NameOf(metabolites.biocyc)) = meta.biocyc)
+        End If
+        If top_main.wikipedia.StringEmpty AndAlso Not meta.wikipedia.StringEmpty Then
+            top_main.wikipedia = meta.wikipedia
+
+            Yield (field(NameOf(metabolites.wikipedia)) = meta.wikipedia)
+        End If
+        If top_main.chebi_id = 0 AndAlso meta.chebi_id > 0 Then
+            top_main.chebi_id = meta.chebi_id
+
+            Yield (field(NameOf(metabolites.chebi_id)) = meta.chebi_id)
+        End If
+        If top_main.pubchem_cid = 0 AndAlso meta.pubchem_cid > 0 Then
+            top_main.pubchem_cid = meta.pubchem_cid
+
+            Yield (field(NameOf(metabolites.pubchem_cid)) = meta.pubchem_cid)
+        End If
+        If top_main.note.StringEmpty AndAlso Not meta.note.StringEmpty Then
+            top_main.note = meta.note
+
+            Yield (field(NameOf(metabolites.note)) = meta.note)
+        End If
+    End Function
+
+    <Extension>
     Public Sub MetaboliteLinks(registry As biocad_registry, Optional check_hash As Boolean = False)
         Dim trans As CommitTransaction = registry.metabolites.open_transaction
 
@@ -231,86 +342,10 @@ Public Module ImportsMetabolite
         End If
 
         For Each hashcode As String In TqdmWrapper.Wrap(registry.metabolites.group_by("hashcode").having(field("*").count > 1).project(Of String)("hashcode"))
-            Dim metas = registry.metabolites.where(field("hashcode") = hashcode).select(Of metabolites)
-            Dim top_main As metabolites = metas.OrderBy(Function(a) a.id).First
-            Dim struct = registry.struct_data.where(field("metabolite_id") = top_main.id).find(Of struct_data)
-
-            For Each meta As metabolites In metas
-                If std.Abs(meta.exact_mass - top_main.exact_mass) > 0.1 Then
-                    Continue For
-                End If
-
-                If meta.id = top_main.id Then
-                    Call trans.add(registry.metabolites.where(field("id") = top_main.id).save_sql(field("main_id") = 0))
-                Else
-                    Dim updates As New List(Of FieldAssert)
-
-                    Call trans.add(registry.metabolites.where(field("id") = meta.id).save_sql(field("main_id") = top_main.id))
-
-                    If top_main.cas_id.StringEmpty AndAlso Not meta.cas_id.StringEmpty Then
-                        Call updates.Add(field(NameOf(metabolites.cas_id)) = meta.cas_id)
-                        top_main.cas_id = meta.cas_id
-                    End If
-                    If top_main.kegg_id.StringEmpty AndAlso Not meta.kegg_id.StringEmpty Then
-                        Call updates.Add(field(NameOf(metabolites.kegg_id)) = meta.kegg_id)
-                        top_main.kegg_id = meta.kegg_id
-                    End If
-                    If top_main.hmdb_id.StringEmpty AndAlso Not meta.hmdb_id.StringEmpty Then
-                        Call updates.Add(field(NameOf(metabolites.hmdb_id)) = meta.hmdb_id)
-                        top_main.hmdb_id = meta.hmdb_id
-                    End If
-                    If top_main.lipidmaps_id.StringEmpty AndAlso Not meta.lipidmaps_id.StringEmpty Then
-                        Call updates.Add(field(NameOf(metabolites.lipidmaps_id)) = meta.lipidmaps_id)
-                        top_main.lipidmaps_id = meta.lipidmaps_id
-                    End If
-                    If top_main.drugbank_id.StringEmpty AndAlso Not meta.drugbank_id.StringEmpty Then
-                        Call updates.Add(field(NameOf(metabolites.drugbank_id)) = meta.drugbank_id)
-                        top_main.drugbank_id = meta.drugbank_id
-                    End If
-                    If top_main.mesh_id.StringEmpty AndAlso Not meta.mesh_id.StringEmpty Then
-                        Call updates.Add(field(NameOf(metabolites.mesh_id)) = meta.mesh_id)
-                        top_main.mesh_id = meta.mesh_id
-                    End If
-                    If top_main.biocyc.StringEmpty AndAlso Not meta.biocyc.StringEmpty Then
-                        Call updates.Add(field(NameOf(metabolites.biocyc)) = meta.biocyc)
-                        top_main.biocyc = meta.biocyc
-                    End If
-                    If top_main.wikipedia.StringEmpty AndAlso Not meta.wikipedia.StringEmpty Then
-                        Call updates.Add(field(NameOf(metabolites.wikipedia)) = meta.wikipedia)
-                        top_main.wikipedia = meta.wikipedia
-                    End If
-                    If top_main.chebi_id = 0 AndAlso meta.chebi_id > 0 Then
-                        Call updates.Add(field(NameOf(metabolites.chebi_id)) = meta.chebi_id)
-                        top_main.chebi_id = meta.chebi_id
-                    End If
-                    If top_main.pubchem_cid = 0 AndAlso meta.pubchem_cid > 0 Then
-                        Call updates.Add(field(NameOf(metabolites.pubchem_cid)) = meta.pubchem_cid)
-                        top_main.pubchem_cid = meta.pubchem_cid
-                    End If
-                    If top_main.note.StringEmpty AndAlso Not meta.note.StringEmpty Then
-                        Call updates.Add(field(NameOf(metabolites.note)) = meta.note)
-                        top_main.note = meta.note
-                    End If
-
-                    If updates.Any Then
-                        Call trans.add(registry.metabolites.where(field("id") = top_main.id).save_sql(updates.ToArray))
-                    End If
-
-                    If struct Is Nothing Then
-                        Dim smiles_str = registry.struct_data.where(field("metabolite_id") = meta.id).find(Of struct_data)
-
-                        If Not smiles_str Is Nothing Then
-                            Call registry.struct_data.add(
-                                field("metabolite_id") = top_main.id,
-                                field("checksum") = smiles_str.checksum,
-                                field("smiles") = smiles_str.smiles
-                            )
-
-                            struct = smiles_str
-                        End If
-                    End If
-                End If
-            Next
+            Call registry.metabolites _
+                .where(field("hashcode") = hashcode) _
+                .select(Of metabolites) _
+                .ResolveMetaboliteAlias(registry, trans)
         Next
 
         Call trans.commit()
