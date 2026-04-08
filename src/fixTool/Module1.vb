079cfad9600e4b331ad8019e95b42f6c23cfe651
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-08 11:15:24 +0800

    config run test

---
+++ src/fixTool/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports registry_data
+
+Module Module1
+
+    ReadOnly names_test As XElement =
+        <names>
+
+        </names>
+
+    Sub cleanNameTest()
+        For Each name As String In names_test.ToString.LineTokens.Select(AddressOf Strings.Trim)
+            Call Console.WriteLine($"{name}{vbTab}{vbTab}{RegisterSymbol.CleanName(name)}")
+        Next
+    End Sub
+End Module
