484ed81db675660805ff8f2bbb089799b2d14bd3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-14 23:24:21 +0800

    load data success

--- src/fixTool/fixRhea.vb
+++ src/fixTool/fixRhea.vb
@@ -8,7 +8,7 @@ Module fixRhea
         For Each filepath As String In ls - l - r - "*.owl" <= "D:\datapool\reactions\chunks"
             Dim xml As biopax = biopax.LoadDoc(filepath)
             Dim loader As SMRUCC.genomics.Model.Biopax.Level3.ResourceReader = SMRUCC.genomics.Model.Biopax.Level3.ResourceReader.LoadResource(file:=xml)
-            Dim reactions = loader.GetAllReactions.ToArray
+            Dim reactions = loader.GetAllReactions(entity_refs:=True).ToArray
             Dim compounds = loader.GetAllCompounds.ToArray
 
             Pause()
