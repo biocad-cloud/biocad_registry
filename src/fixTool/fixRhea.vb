5d1585d959148aba8809c272d9896dfe85f79a00
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-14 22:30:36 +0800

    fix of the loader warning

--- src/fixTool/fixRhea.vb
+++ src/fixTool/fixRhea.vb
@@ -1,13 +1,16 @@
 ﻿Imports Microsoft.VisualBasic.Linq
 Imports biopax = SMRUCC.genomics.Model.Biopax.Level3.File
+Imports Microsoft.VisualBasic.Language.UnixBash
 
 Module fixRhea
 
     Sub Main2()
-        Dim xml As biopax = biopax.LoadDoc("D:\datapool\rhea-biopax.owl")
-        Dim loader As SMRUCC.genomics.Model.Biopax.Level3.ResourceReader = SMRUCC.genomics.Model.Biopax.Level3.ResourceReader.LoadResource(file:=xml)
-        Dim reactions = loader.GetAllReactions.ToArray
+        For Each filepath As String In ls - l - r - "*.owl" <= "D:\datapool\reactions\chunks"
+            Dim xml As biopax = biopax.LoadDoc(filepath)
+            Dim loader As SMRUCC.genomics.Model.Biopax.Level3.ResourceReader = SMRUCC.genomics.Model.Biopax.Level3.ResourceReader.LoadResource(file:=xml)
+            Dim reactions = loader.GetAllReactions.ToArray
 
-        Pause()
+            Pause()
+        Next
     End Sub
 End Module
